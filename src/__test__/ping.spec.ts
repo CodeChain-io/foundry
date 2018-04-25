2198f48874703135f3c4268f51e29d79aad593aa
JinGyeong Jeong <jjg@kodebox.io>
2018-04-25 21:46:30 +0900

    Add test for ping()

---
+++ src/__test__/ping.spec.ts
@@ -0,0 +1,9 @@
+import * as SDK from "../";
+
+const SERVER_URL = "http://localhost:8080";
+
+test('ping', async () => {
+    new SDK(SERVER_URL).ping().then(res => {
+        expect(res).toBe("pong");
+    });
+}, 100);
