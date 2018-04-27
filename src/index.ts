a4f68e15faacae4d23a9b389ee7caa1128041235
JinGyeong Jeong <jjg@kodebox.io>
2018-04-27 15:12:45 +0900

    Add sendSignedTransaction() to SDK

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import SignedTransaction from "./primitives/SignedTransaction";
+import H256 from "./primitives/H256";
+import Transaction from "./primitives/Transaction";
+import U256 from "./primitives/U256";
+import Action from "./primitives/Action";
+
 const jayson = require('jayson');
 
 class SDK {
@@ -15,6 +21,20 @@ class SDK {
                 }
                 resolve(res.result);
             });
+        });
+    }
+
+    sendSignedTransaction(t: SignedTransaction): Promise<H256> {
+        return new Promise((resolve, reject) => {
+            const bytes = Array.from(t.rlpBytes()).map(byte => byte < 0x10 ? `0${byte.toString(16)}` : byte.toString(16)).join("");
+            this.client.request("chain_sendSignedTransaction", [`0x${bytes}`], (err, res) => {
+                if (err) {
+                    return reject(err);
+                } else if (res.error) {
+                    return reject(res.error);
+                }
+                resolve(new H256(res.result));
+            })
         });
     }
 }
