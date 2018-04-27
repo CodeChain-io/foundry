f72e8d669ee7f824e127d14098bc668c0ad65df3
JinGyeong Jeong <jjg@kodebox.io>
2018-04-27 17:06:59 +0900

    Add Invoice primitive and its rlp test

---
+++ src/primitives/Invoice.ts
@@ -0,0 +1,29 @@
+const RLP = require("rlp");
+
+type TransactionOutcome = "Success" | "Failed";
+
+class Invoice {
+    private outcome: TransactionOutcome;
+
+    constructor(success: boolean) {
+        this.outcome = success ? "Success" : "Failed";
+    }
+
+    static fromBytes(buffer: Buffer): Invoice {
+        const bytes = Array.from(buffer.values());
+        if (bytes.length !== 1 || bytes[0] > 0x01) {
+            throw `Invalid RLP for Invoice: ${bytes}`;
+        }
+        return new Invoice(RLP.decode(buffer)[0]);
+    }
+
+    toEncodeObject(): boolean {
+        return this.outcome === "Success";
+    }
+
+    rlpBytes(): Buffer {
+        return Buffer.from([this.toEncodeObject() ? 0x01 : 0x00])
+    }
+}
+
+export default Invoice;
---
+++ src/primitives/__test__/Invoice.spec.ts
@@ -0,0 +1,9 @@
+import Invoice from "../Invoice";
+
+test("rlp", () => {
+    expect(new Invoice(true).rlpBytes()).toEqual(Buffer.from([0x01]));
+    expect(new Invoice(false).rlpBytes()).toEqual(Buffer.from([0x00]));
+
+    expect(Invoice.fromBytes(Buffer.from([0x01]))).toEqual(new Invoice(true));
+    expect(Invoice.fromBytes(Buffer.from([0x00]))).toEqual(new Invoice(false));
+});
