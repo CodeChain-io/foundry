3afdc0eb8b91c1ee502364f37ffaeff12045e496
JinGyeong Jeong <jjg@kodebox.io>
2018-04-27 12:50:00 +0900

    Add rlpBytes() and hash() for SignedTransaction and its test

--- src/primitives/SignedTransaction.ts
+++ src/primitives/SignedTransaction.ts
@@ -1,5 +1,9 @@
 import U256 from "./U256";
+import H256 from "./H256";
 import Transaction from "./Transaction";
+
+const blake = require("blakejs");
+const RLP = require("rlp");
 
 class SignedTransaction {
     private unsigned: Transaction;
@@ -9,7 +13,7 @@ class SignedTransaction {
 
     constructor(unsigned: Transaction, v: number, r: U256, s: U256) {
         this.unsigned = unsigned;
-        this.v = v;
+        this.v = v + 27;
         this.r = r;
         this.s = s;
     }
@@ -17,6 +21,31 @@ class SignedTransaction {
     signature() {
         const { v, r, s } = this;
         return { v, r, s };
+    }
+
+    toEncodeObject(): Array<any> {
+        const { unsigned: { nonce, fee, action, networkId }, v, r, s } = this;
+        return [
+            nonce.toEncodeObject(),
+            fee.toEncodeObject(),
+            action.toEncodeObject(),
+            networkId.toEncodeObject(),
+            v,
+            r.toEncodeObject(),
+            s.toEncodeObject()
+        ];
+    }
+
+    rlpBytes(): Buffer {
+        return RLP.encode(this.toEncodeObject());
+    }
+
+    hash(): H256 {
+        const context = blake.blake2bInit(32, null);
+        blake.blake2bUpdate(context, this.rlpBytes());
+        let hash: Buffer = blake.blake2bFinal(context);
+        let hashStr = Array.from(hash).map(byte => byte < 0x10 ? `0${byte.toString(16)}` : byte.toString(16)).join("");
+        return new H256(hashStr);
     }
 }
 
--- src/primitives/__test__/Transaction.spec.ts
+++ src/primitives/__test__/Transaction.spec.ts
@@ -17,7 +17,13 @@ test("sign", () => {
     const t = new Transaction(new U256(0), new U256(0), new Action("noop"), 1);
     const signed = t.sign(new H256("ede1d4ccb4ec9a8bbbae9a13db3f4a7b56ea04189be86ac3a6a439d9a0a1addd"));
     const { v, r, s } = signed.signature();
-    expect(v).toBe(1);
+    expect(v).toBe(1 + 27);
     expect(r).toEqual(new U256("0x452361fcdbb033ad9cc8d70a5fb18f72e100fffd7175adfca72a5ea5339ce505"));
     expect(s).toEqual(new U256("0x1294f76e5fbe2cc8fdfb3bcb34253de92678bf7f65646a6dfa58bb0c6d83c839"));
+});
+
+test("signed hash", () => {
+    const t = new Transaction(new U256(0), new U256(0), new Action("noop"), 1);
+    const signed = t.sign(new H256("ede1d4ccb4ec9a8bbbae9a13db3f4a7b56ea04189be86ac3a6a439d9a0a1addd"));
+    expect(signed.hash()).toEqual(new H256("20f6fffaf8c3252f683b255dbb8fcf7ffacdb69638b2a9513429fb83342f4a06"));
 });
