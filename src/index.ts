e82e8e2ef68af9935eebe66778a75159fc4bb252
JinGyeong Jeong <jjg@kodebox.io>
2018-04-27 17:35:04 +0900

    Add getTransactionInvoice() to SDK

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import H256 from "./primitives/H256";
 import Transaction from "./primitives/Transaction";
 import U256 from "./primitives/U256";
 import Action from "./primitives/Action";
+import Invoice from "./primitives/Invoice";
 
 const jayson = require('jayson');
 
@@ -35,6 +36,20 @@ class SDK {
                 }
                 resolve(new H256(res.result));
             })
+        });
+    }
+
+    // FIXME: timeout not implemented
+    getTransactionInvoice(txhash: H256, _timeout: number): Promise<Invoice | null> {
+        return new Promise((resolve, reject) => {
+            this.client.request("chain_getTransactionInvoice", [`0x${txhash.value}`], (err, res) => {
+                if (err) {
+                    return reject(err);
+                } else if (res.error) {
+                    return reject(res.error);
+                }
+                resolve(new Invoice(res.result.outcome === "Success"));
+            });;
         });
     }
 }
