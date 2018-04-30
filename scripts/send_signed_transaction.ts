f479e0171fc5e5b647633e1753f22417edce7c2c
JinGyeong Jeong <jjg@kodebox.io>
2018-04-30 15:55:15 +0900

    Update example script for Payment action

--- scripts/send_signed_transaction.ts
+++ scripts/send_signed_transaction.ts
@@ -8,12 +8,17 @@ const networkId = Number.parseInt(process.env.NETWORK_ID) || 17;
 
 const sdk = new SDK("http://localhost:8080");
 
+const noopAction = new Action("noop", {});
+const paymentAction = new Action("payment", {
+    address: new H160("3f4aa1fedf1f54eeb03b759deadb36676b184911"),
+    value: new U256("10")
+});
+
 sdk.getNonce(address).then(nonce => {
     console.log(nonce);
 
     const fee = new U256(10);
-    const action = new Action("noop");
-    const t = new Transaction(nonce, fee, action, networkId);
+    const t = new Transaction(nonce, fee, paymentAction, networkId);
     return sdk.sendSignedTransaction(t.sign(secret));
 }).then(hash => {
     console.log(hash);
