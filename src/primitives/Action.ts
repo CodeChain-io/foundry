319a5b9d0c9554266466fce657e486ed844f9272
JinGyeong Jeong <jjg@kodebox.io>
2018-04-30 15:54:56 +0900

    Support Payment action

--- src/primitives/Action.ts
+++ src/primitives/Action.ts
@@ -1,18 +1,34 @@
+import { H160, U256 } from "./index";
+
 const RLP = require("rlp");
 
-// FIXME: Support "payment", "set_regular_key", "asset_mint" and etc.
-export type ActionType = "noop";
+type NoopActionData = {};
+type PaymentActionData = {
+    address: H160;
+    value: U256;
+};
+
+// FIXME: Support "set_regular_key", "asset_mint" and etc.
+export type ActionType = "noop" | "payment";
+export type ActionData = NoopActionData | PaymentActionData;
 
 export class Action {
     type: ActionType;
+    data: ActionData;
 
-    constructor(type: ActionType) {
+    constructor(type: ActionType, data: ActionData) {
         this.type = type;
+        this.data = data;
     }
 
-    toEncodeObject(): string {
-        // FIXME: noop hard-coded here.
-        return "";
+    toEncodeObject(): string | Array<any> {
+        switch(this.type) {
+            case "noop":
+                return "";
+            case "payment":
+                const { address, value } = this.data as PaymentActionData;
+                return [1, address.toEncodeObject(), value.toEncodeObject()];
+        }
     }
 
     rlpBytes(): Buffer {
