434dfcb9260138d6aeac96c1c89da8f2aeda33c7
JinGyeong Jeong <jjg@kodebox.io>
2018-05-23 14:58:31 +0900

    Fix ping test passes even if server is unavailable

--- integration_tests/ping.spec.ts
+++ integration_tests/ping.spec.ts
@@ -3,7 +3,6 @@ import { SDK } from "../";
 const SERVER_URL = "http://localhost:8080";
 
 test("ping", async () => {
-    new SDK(SERVER_URL).ping().then(res => {
-        expect(res).toBe("pong");
-    });
-}, 100);
+    const response = await new SDK(SERVER_URL).ping();
+    expect(response).toBe("pong");
+});
