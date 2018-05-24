04c28e537476733da4d90e74b1e0328cd61a7e13
JinGyeong Jeong <jjg@kodebox.io>
2018-05-24 15:51:51 +0900

    Change getPendingParcels to return if the pending already exists

--- integration_tests/getPendingParcels.spec.ts
+++ integration_tests/getPendingParcels.spec.ts
@@ -5,7 +5,14 @@ const SERVER_URL = "http://localhost:8080";
 const sdk = new SDK(SERVER_URL);
 
 test("getPendingParcels", async () => {
+    let pending = await sdk.getPendingParcels();
+    if (pending.length > 0) {
+        // FIXME: test Parcel type
+        expect(pending[0].transaction).toBeTruthy();
+        return;
+    }
     await sendNoopTwice();
-    const pending = await sdk.getPendingParcels();
+    pending = await sdk.getPendingParcels();
+    // FIXME: test Parcel type
     expect(pending[0].transaction).toEqual("noop");
 });
