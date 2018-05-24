d03f34b1bd1ff3d08741eb630c3c5bd3d8b2d337
JinGyeong Jeong <jjg@kodebox.io>
2018-05-24 21:07:04 +0900

    Add getParcel spec

---
+++ integration_tests/getParcel.spec.ts
@@ -0,0 +1,20 @@
+import { SDK, Parcel, U256 } from "../";
+import { payment } from "./helper";
+
+const SERVER_URL = "http://localhost:8080";
+const sdk = new SDK(SERVER_URL);
+
+test("getParcel", async () => {
+    const hash = await payment();
+    const parcel = await sdk.getParcel(hash);
+    expect(parcel).toMatchObject({
+        fee: expect.any(U256),
+        nonce: expect.any(U256),
+        networkId: expect.any(U256),
+        transaction: expect.objectContaining({
+            type: expect.anything(),
+            data: expect.anything(),
+        }),
+    });
+});
+
