33f0e9cc857335cf50ee0a3259e245adb91a8839
JinGyeong Jeong <jjg@kodebox.io>
2018-06-26 20:20:19 +0900

    Add AssetTransferAddress and its tests

---
+++ src/AssetTransferAddress.ts
@@ -0,0 +1,92 @@
+import { H256 } from "./primitives/H256";
+import { encode, toWords, decode, fromWords } from "./bech32";
+import { toHex } from "./utils";
+
+const LOCK_SCRIPT_HASH_TYPE = 0x00;
+const PAY_TO_PUBLIC_KEY_HASH_TYPE = 0x01;
+
+/**
+ * Substitutes for asset owner data which consists of network id,
+ * lockScriptHash, parameters. The network id is represented with prefix
+ * "cca"(mainnet) or "tca"(testnet). Currently version 0 exists only.
+ *
+ * Refer to the wiki for the details about AssetTransferAddress.
+ * https://github.com/CodeChain-io/codechain/wiki/CodeChain-Address
+ */
+export class AssetTransferAddress {
+    type: number;
+    payload: H256;
+
+    value: string;
+
+    private constructor(type: number, payload: H256, address: string) {
+        this.type = type;
+        this.payload = payload;
+        this.value = address;
+    }
+
+    static fromTypeAndPayload(type: number, payload: H256, options: { isTestnet?: boolean, version?: number } = {}) {
+        const { isTestnet = false, version = 0 } = options;
+
+        if (version !== 0) {
+            throw `Unsupported version for asset transfer address: ${version}`;
+        }
+
+        if (type < 0x00 || type > 0x01) {
+            throw `Unsupported type for asset transfer address: ${type}`;
+        }
+
+        const words = toWords(Buffer.from([version, type, ...Buffer.from(payload.value, "hex")]));
+        const address = encode(isTestnet ? "tca" : "cca", words);
+        return new AssetTransferAddress(type, payload, address);
+    }
+
+    static fromLockScriptHash(lockScriptHash: H256, options: { isTestnet?: boolean, version?: number } = {}) {
+        const { isTestnet = false, version = 0 } = options;
+        const type = LOCK_SCRIPT_HASH_TYPE;
+
+        if (version !== 0) {
+            throw `Unsupported version for asset transfer address: ${version}`;
+        }
+
+        const words = toWords(Buffer.from([version, type, ...Buffer.from(lockScriptHash.value, "hex")]));
+        const address = encode(isTestnet ? "tca" : "cca", words);
+        return new AssetTransferAddress(type, lockScriptHash, address);
+    }
+
+    static fromPublicKeyHash(publicKeyHash: H256, options: { isTestnet?: boolean, version?: number } = {}) {
+        const { isTestnet = false, version = 0 } = options;
+        const type = PAY_TO_PUBLIC_KEY_HASH_TYPE;
+
+        if (version !== 0) {
+            throw `Unsupported version for asset transfer address: ${version}`;
+        }
+
+        const words = toWords(Buffer.from([version, type, ...Buffer.from(publicKeyHash.value, "hex")]));
+        const address = encode(isTestnet ? "tca" : "cca", words);
+        return new AssetTransferAddress(type, publicKeyHash, address);
+    }
+
+    static fromAddress(address: string) {
+        if (!address.startsWith("cca") && !address.startsWith("tca")) {
+            throw `The prefix is unknown for asset transfer address: ${address}`;
+        }
+
+        const { words } = decode(address, address.substr(0, 3));
+        const bytes = fromWords(words);
+        const version = bytes[0];
+
+        if (version !== 0) {
+            throw `Unsupported version for asset transfer address: ${version}`;
+        }
+
+        const type = bytes[1];
+
+        if (type < 0x00 || type > 0x01) {
+            throw `Unsupported type for asset transfer address: ${type}`;
+        }
+
+        const payload = toHex(Buffer.from(bytes.slice(2)));
+        return new this(type, new H256(payload), address);
+    }
+}
---
+++ src/__test__/AssetTransferAddress.spec.ts
@@ -0,0 +1,50 @@
+import { AssetTransferAddress } from "../AssetTransferAddress";
+import { H256 } from "../primitives/H256";
+
+test("AssetTransferAddress.fromLockScriptHash - mainnet", () => {
+    const lockScriptHash = new H256("50a2c0d145539c1fb32f60e0d8425b1c03f6120c40171971b8de9c0017a4bfb3");
+    const address = AssetTransferAddress.fromLockScriptHash(lockScriptHash);
+    expect(address.value).toMatch(/^cca[a-z0-9]+$/);
+});
+
+test("AssetTransferAddress.fromLockScriptHash - testnet", () => {
+    const lockScriptHash = new H256("50a2c0d145539c1fb32f60e0d8425b1c03f6120c40171971b8de9c0017a4bfb3");
+    const address = AssetTransferAddress.fromLockScriptHash(lockScriptHash, { isTestnet: true });
+    expect(address.value).toMatch(/^tca[a-z0-9]+$/);
+});
+
+test("AssetTransferAddress.fromLockScriptHash - invalid version", () => {
+    const lockScriptHash = new H256("50a2c0d145539c1fb32f60e0d8425b1c03f6120c40171971b8de9c0017a4bfb3");
+    expect(() => {
+        AssetTransferAddress.fromLockScriptHash(lockScriptHash, { version: 1 });
+    }).toThrow("Unsupported version for asset transfer address: 1");
+});
+
+test("AssetTransferAddress.fromPublicKeyHash - mainnet", () => {
+    const publicKeyHash = new H256("50a2c0d145539c1fb32f60e0d8425b1c03f6120c40171971b8de9c0017a4bfb3");
+    const address = AssetTransferAddress.fromPublicKeyHash(publicKeyHash);
+    expect(address.value).toMatch(/^cca[a-z0-9]+$/);
+});
+
+test("AssetTransferAddress.fromPublicKeyHash - testnet", () => {
+    const publicKeyHash = new H256("50a2c0d145539c1fb32f60e0d8425b1c03f6120c40171971b8de9c0017a4bfb3");
+    const address = AssetTransferAddress.fromPublicKeyHash(publicKeyHash, { isTestnet: true });
+    expect(address.value).toMatch(/^tca[a-z0-9]+$/);
+});
+
+test("AssetTransferAddress.fromAddress - mainnet", () => {
+    const address = AssetTransferAddress.fromAddress("ccaqqq9pgkq69z488qlkvhkpcxcgfd3cqlkzgxyq9cewxuda8qqz7jtlvcev083x");
+    expect(address.payload).toEqual(new H256("50a2c0d145539c1fb32f60e0d8425b1c03f6120c40171971b8de9c0017a4bfb3"));
+});
+
+test("AssetTransferAddress.fromAddress - testnet", () => {
+    const address = AssetTransferAddress.fromAddress("tcaqqq9pgkq69z488qlkvhkpcxcgfd3cqlkzgxyq9cewxuda8qqz7jtlvctt5eze");
+    expect(address.payload).toEqual(new H256("50a2c0d145539c1fb32f60e0d8425b1c03f6120c40171971b8de9c0017a4bfb3"));
+});
+
+test("AssetTransferAddress.fromAddress - invalid checksum", () => {
+    expect(() => {
+        AssetTransferAddress.fromAddress("ccaqqq9pgkq69z488qlkvhkpcxcgfd3cqlkzgxyq9cewxuda8qqz7jtlvcqqqqqq");
+    }).toThrow();
+});
+
