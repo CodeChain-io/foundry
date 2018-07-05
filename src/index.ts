7934c4bc967df2691c12e5acb48fe460517f0081
Juhyeong Park <jh@kodebox.io>
2018-07-05 14:05:49 +0900

    Add shareSecret() and connect() to SDK

--- src/index.ts
+++ src/index.ts
@@ -353,6 +353,33 @@ class SDK {
         });
     }
 
+    /**
+     * Save secret which is used when handshaking with other node,
+     * This secret may be exchanged in offline.
+     * To use this saved secret, you should call 'net_connect' RPC after this RPC call.
+     * @param secret Secret exchanged in offline
+     * @param address Node address which RPC server will connect to using secret
+     * @param port
+     */
+    shareSecret(secret: string, address: string, port: number): Promise<null> {
+        return this.sendRpcRequest(
+            "net_shareSecret",
+            [secret, address, port]
+        );
+    }
+
+    /**
+     * Connect to node
+     * @param address Node address which to connect
+     * @param port
+     */
+    connect(address: string, port: number): Promise<null> {
+        return this.sendRpcRequest(
+            "net_connect",
+            [address, port]
+        );
+    }
+
     // Primitives
     static SDK = SDK;
     static H160 = H160;
