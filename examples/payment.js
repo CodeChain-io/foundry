1a45165148a3da001d72a5499954fa1924a19608
JinGyeong Jeong <jjg@kodebox.io>
2018-07-23 18:21:33 +0900

    Update payment example

--- examples/payment.js
+++ examples/payment.js
@@ -2,19 +2,16 @@ var SDK = require("codechain-sdk");
 
 var sdk = new SDK({ server: "http://localhost:8080" });
 
-var signerSecret = "ede1d4ccb4ec9a8bbbae9a13db3f4a7b56ea04189be86ac3a6a439d9a0a1addd"
+var parcel = sdk.core.createPaymentParcel({
+    recipient: "0x744142069fe2d03d48e61734cbe564fcc94e6e31",
+    amount: 10000
+});
 
-sdk.rpc.account.importRaw(signerSecret).then(account => {
-    var parcel = sdk.core.createPaymentParcel({
-        recipient: "0x744142069fe2d03d48e61734cbe564fcc94e6e31",
-        amount: 10000,
-    });
-    return sdk.rpc.chain.sendParcel(parcel, { account });
+sdk.rpc.chain.sendParcel(parcel, {
+    account: "0xa6594b7196808d161b6fb137e781abbc251385d9",
+    passphrase: "satoshi"
 }).then(function (parcelHash) {
-    console.log("Parcel Hash: ", parcelHash);
     return sdk.rpc.chain.getParcelInvoice(parcelHash, { timeout: 5 * 60 * 1000 });
-}).then(function (invoice) {
-    console.log("Parcel Invoice: ", invoice);
-}).catch((err) => {
-    console.error(`Error:`, err);
+}).then(function (parcelInvoice) {
+    console.log(parcelInvoice) // { success: true }
 });
