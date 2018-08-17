2abf164d20f33b89e319f4e2cf7afd7a6ad9e578
JinGyeong Jeong <jjg@kodebox.io>
2018-08-17 16:09:32 +0900

    Refactor the P2PKH test

--- src/key/__test__/P2PKH.spec.ts
+++ src/key/__test__/P2PKH.spec.ts
@@ -1,5 +1,9 @@
+import { H256 } from "../../../lib/core/H256";
+import { blake256 } from "../../utils";
+
 import { P2PKH } from "../P2PKH";
 
 test("getLockScriptHash()", () => {
-    expect(P2PKH.getLockScriptHash().value).toEqual("f42a65ea518ba236c08b261c34af0521fa3cd1aa505e1c18980919cb8945f8f3");
+    const hash = new H256(blake256(P2PKH.getLockScript()));
+    expect(P2PKH.getLockScriptHash()).toEqual(hash);
 });
