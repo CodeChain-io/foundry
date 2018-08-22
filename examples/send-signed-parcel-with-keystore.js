605822cb0919fd351fd3009cba6fe55fce8fc86c
JinGyeong Jeong <jjg@kodebox.io>
2018-08-22 19:09:00 +0900

    Add send-signed-parcel-with-keystore example

---
+++ examples/send-signed-parcel-with-keystore.js
@@ -0,0 +1,19 @@
+var SDK = require("..");
+
+var sdk = new SDK({ server: "http://localhost:8080" });
+
+var parcel = sdk.core.createPaymentParcel({
+    recipient: "tccqruq09sfgax77nj4gukjcuq69uzeyv0jcs7vzngg",
+    amount: 10000,
+});
+
+(async () => {
+    const keyStore = await sdk.key.createRemoteKeyStore("http://localhost:7007");
+    const account = await sdk.key.createPlatformAddress({ keyStore });
+    const nonce = await sdk.rpc.chain.getNonce(account);
+
+    const signedParcel = await sdk.key.signParcel(parcel, { account, keyStore, fee: 10, nonce });
+    console.log(signedParcel);
+    // FIXME: needs fee
+    // const parcelHash = await sdk.rpc.chain.sendSignedParcel(signedParcel);
+})();
