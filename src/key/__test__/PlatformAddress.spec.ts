9016b15cefa7cb031f18673e0878864e8d2527c6
JinGyeong Jeong <jjg@kodebox.io>
2018-08-31 15:44:30 +0900

    Cleanup PlatformAddress.spec.ts

--- src/key/__test__/PlatformAddress.spec.ts
+++ src/key/__test__/PlatformAddress.spec.ts
@@ -2,56 +2,56 @@ import { H160 } from "../../core/H160";
 
 import { PlatformAddress } from "../PlatformAddress";
 
-test("PlatformAddress.fromAccountId - mainnet (default)", () => {
+describe("fromAccountId", () => {
     const accountId = new H160("7b5e0ee8644c6f585fc297364143280a45844502");
-    const address = PlatformAddress.fromAccountId(accountId, {
-        networkId: "cc"
+
+    test("mainnet", () => {
+        const address = PlatformAddress.fromAccountId(accountId, {
+            networkId: "cc"
+        });
+        expect(address.value).toMatch(/^ccc[a-z0-9]+$/);
     });
-    expect(address.value).toMatch(/^ccc[a-z0-9]+$/);
+
+    test("testnet", () => {
+        const address = PlatformAddress.fromAccountId(accountId);
+        expect(address.value).toMatch(/^tcc[a-z0-9]+$/);
+    });
+
+    test("Valid version", () => {
+        expect(() => {
+            PlatformAddress.fromAccountId(accountId, { version: 0 });
+        }).not.toThrow();
+    });
+
+    test("Invalid version", () => {
+        expect(() => {
+            PlatformAddress.fromAccountId(accountId, { version: 1 });
+        }).toThrow("Unsupported version for platform address: 1");
+    });
 });
 
-test("PlatformAddress.fromAccountId - testnet", () => {
-    const accountId = new H160("7b5e0ee8644c6f585fc297364143280a45844502");
-    const address = PlatformAddress.fromAccountId(accountId);
-    expect(address.value).toMatch(/^tcc[a-z0-9]+$/);
-});
+describe("fromString", () => {
+    const accountId = "7b5e0ee8644c6f585fc297364143280a45844502";
+    const mainnetAddress = "cccqpa4urhgv3xx7kzlc2tnvs2r9q9ytpz9qg4aw2qm";
+    const testnetAddress = "tccqpa4urhgv3xx7kzlc2tnvs2r9q9ytpz9qga6ufnz";
 
-test("PlatformAddress.fromAccountId - valid version", () => {
-    const accountId = new H160("7b5e0ee8644c6f585fc297364143280a45844502");
-    expect(() => {
-        PlatformAddress.fromAccountId(accountId, { version: 0 });
-    }).not.toThrow();
-});
+    test("mainnet", () => {
+        const address = PlatformAddress.fromString(mainnetAddress);
+        expect(address.accountId).toEqual(new H160(accountId));
+    });
 
-test("PlatformAddress.fromAccountId - invalid version", () => {
-    const accountId = new H160("7b5e0ee8644c6f585fc297364143280a45844502");
-    expect(() => {
-        PlatformAddress.fromAccountId(accountId, { version: 1 });
-    }).toThrow("Unsupported version for platform address: 1");
-});
+    test("testnet", () => {
+        const address = PlatformAddress.fromString(testnetAddress);
+        expect(address.accountId).toEqual(new H160(accountId));
+    });
 
-test("PlatformAddress.fromString - mainnet", () => {
-    const address = PlatformAddress.fromString(
-        "cccqpa4urhgv3xx7kzlc2tnvs2r9q9ytpz9qg4aw2qm"
-    );
-    expect(address.accountId).toEqual(
-        new H160("7b5e0ee8644c6f585fc297364143280a45844502")
-    );
-});
-
-test("PlatformAddress.fromString - testnet", () => {
-    const address = PlatformAddress.fromString(
-        "tccqpa4urhgv3xx7kzlc2tnvs2r9q9ytpz9qga6ufnz"
-    );
-    expect(address.accountId).toEqual(
-        new H160("7b5e0ee8644c6f585fc297364143280a45844502")
-    );
-});
-
-test("PlatformAddress.fromString - invalid checksum", () => {
-    expect(() => {
-        PlatformAddress.fromString(
-            "cccqpa4urhgv3xx7kzlc2tnvs2r9q9ytpz9qgqqqqqq"
-        );
-    }).toThrow();
+    test("Invalid checksum", () => {
+        const invalidChecksumAddress =
+            "cccqpa4urhgv3xx7kzlc2tnvs2r9q9ytpz9qgqqqqqq";
+        try {
+            PlatformAddress.fromString(invalidChecksumAddress);
+        } catch (e) {
+            expect(e.toString()).toContain("checksum");
+        }
+    });
 });
