ff26ff13ee969cc10e581d068ee3462d04d23eda
JinGyeong Jeong <jjg@kodebox.io>
2018-09-07 19:34:53 +0900

    Add tests for getRegularKeyOwner()

---
+++ src/rpc/__test__/invalid-argument.spec.ts
@@ -0,0 +1,24 @@
+import { Rpc } from "..";
+
+describe("Invalid function argument", () => {
+    const rpc = new Rpc({ server: "" });
+
+    describe("getRegularKeyOwner", () => {
+        const regularKey =
+            "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+        const invalidRegularKey = "0x0";
+        const invalidBlockNumber: any = null;
+
+        test("first argument", () => {
+            expect(() => {
+                rpc.chain.getRegularKeyOwner(invalidRegularKey);
+            }).toThrow(invalidRegularKey);
+        });
+
+        test("second argument", () => {
+            expect(() => {
+                rpc.chain.getRegularKeyOwner(regularKey, invalidBlockNumber);
+            }).toThrow(invalidRegularKey);
+        });
+    });
+});
