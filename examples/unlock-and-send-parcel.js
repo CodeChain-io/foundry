1d1092bebe8ef4e7a20d48e5050b95036de110e6
Seulgi Kim <seulgi.kim@kodebox.io>
2018-09-28 20:04:35 +0900

    Add unlock and sendParcel example

---
+++ examples/unlock-and-send-parcel.js
@@ -0,0 +1,35 @@
+var SDK = require("codechain-sdk");
+
+var sdk = new SDK({
+    server: process.env.CODECHAIN_RPC_HTTP || "http://localhost:8080",
+    networkId: process.env.CODECHAIN_NETWORK_ID || "tc"
+});
+
+var ACCOUNT_ADDRESS =
+    process.env.ACCOUNT_ADDRESS ||
+    "tccqzn9jjm3j6qg69smd7cn0eup4w7z2yu9my9a2k78";
+var ACCOUNT_PASSPHRASE = process.env.ACCOUNT_PASSPHRASE || "satoshi";
+
+var parcel = sdk.core.createPaymentParcel({
+    recipient: "tccqruq09sfgax77nj4gukjcuq69uzeyv0jcs7vzngg",
+    amount: 10000
+});
+
+sdk.rpc.account
+    .unlock(ACCOUNT_ADDRESS, ACCOUNT_PASSPHRASE)
+    .then(() => {
+        return sdk.rpc.account
+            .sendParcel({
+                parcel,
+                account: ACCOUNT_ADDRESS
+            })
+            .then(function(result) {
+                return sdk.rpc.chain.getParcelInvoice(result.hash, {
+                    timeout: 300 * 1000
+                });
+            })
+            .then(function(parcelInvoice) {
+                console.log(parcelInvoice); // { success: true }
+            });
+    })
+    .catch(console.error);
