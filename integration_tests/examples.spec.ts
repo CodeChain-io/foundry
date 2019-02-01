59c5b2c3575d334e0d483054ee78a533fdf60095
Seulgi Kim <seulgi.kim@kodebox.io>
2019-02-01 17:15:58 +0900

    Make the integration tests run all files in examples

--- integration_tests/examples.spec.ts
+++ integration_tests/examples.spec.ts
@@ -1,5 +1,5 @@
 import { execFile } from "child_process";
-import { readFileSync, unlinkSync, writeFileSync } from "fs";
+import { readdirSync, readFileSync, unlinkSync, writeFileSync } from "fs";
 
 describe("examples", () => {
     beforeAll(done => {
@@ -9,69 +9,14 @@ describe("examples", () => {
         runExample("import-test-account", done);
     });
 
-    test("create-account-with-rpc", done => {
-        runExample("create-account-with-rpc", done);
-    });
+    const excludes = ["import-test-account"];
+    const tests = readdirSync("examples/")
+        .filter(filename => filename.endsWith(".js"))
+        .map(filename => filename.replace(/.js$/, ""))
+        .filter(filename => !filename.startsWith("test-"))
+        .filter(filename => !excludes.includes(filename));
 
-    test("create-account-with-secret", done => {
-        runExample("create-account-with-secret", done);
-    });
-
-    test("get-balance", done => {
-        runExample("get-balance", done);
-    });
-
-    test("send-tx", done => {
-        runExample("send-tx", done);
-    });
-
-    test("send-signed-tx", done => {
-        runExample("send-signed-tx", done);
-    });
-
-    test("create-asset-transfer-address", done => {
-        runExample("create-asset-transfer-address", done);
-    });
-
-    test("mint-asset", done => {
-        runExample("mint-asset", done);
-    });
-
-    test("mint-and-transfer", done => {
-        runExample("mint-and-transfer", done);
-    });
-
-    test("mint-and-burn", done => {
-        runExample("mint-and-burn", done);
-    });
-
-    test("set-regular-key", done => {
-        runExample("set-regular-key", done);
-    });
-
-    test("mint-and-compose", done => {
-        runExample("mint-and-compose", done);
-    });
-
-    test("mint-and-compose-and-decompose", done => {
-        runExample("mint-and-compose-and-decompose", done);
-    });
-
-    test("wrap-ccc", done => {
-        runExample("wrap-ccc", done);
-    });
-
-    test("wrap-ccc-and-unwrap-ccc", done => {
-        runExample("wrap-ccc-and-unwrap-ccc", done);
-    });
-
-    test("mint-and-transfer-with-order", done => {
-        runExample("mint-and-transfer-with-order", done);
-    });
-
-    test("store-and-remove", done => {
-        runExample("store-and-remove", done);
-    });
+    tests.map(name => test(name, done => runExample(name, done)));
 });
 
 function runExample(name: string, done: () => any) {
