a2b80e236de3239fdd43d1ee392856573ac396f6
JinGyeong Jeong <jjg@kodebox.io>
2019-02-21 18:11:16 +0900

    Add an example for the Custom transaction

---
+++ examples/send-custom-transaction.ts
@@ -0,0 +1,30 @@
+const SDK = require("codechain-sdk");
+
+const RLP = require("rlp");
+
+const sdk = new SDK({
+    server: process.env.CODECHAIN_RPC_HTTP || "http://localhost:8080",
+    networkId: process.env.CODECHAIN_NETWORK_ID || "tc"
+});
+
+const ACCOUNT_ADDRESS =
+    process.env.ACCOUNT_ADDRESS ||
+    "tccq9h7vnl68frvqapzv3tujrxtxtwqdnxw6yamrrgd";
+const ACCOUNT_PASSPHRASE = process.env.ACCOUNT_PASSPHRASE || "satoshi";
+
+(async () => {
+    const hash = await sdk.rpc.chain.sendTransaction(
+        sdk.core.createCustomTransaction({
+            handlerId: 1,
+            bytes: RLP.encode([0])
+        }),
+        {
+            account: ACCOUNT_ADDRESS,
+            passphrase: ACCOUNT_PASSPHRASE
+        }
+    );
+    const invoice = await sdk.rpc.chain.getInvoice(hash);
+    console.log("invoice:", invoice);
+})().catch(err => {
+    console.error(`Error:`, err);
+});
