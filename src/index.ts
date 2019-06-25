33a1aadd6280ebec4fd7c6adea789ce2fe118dfb
SeongChan Lee <sclee@codechain.io>
2019-06-25 15:52:24 +0900

    Implement a method to decode action from RLP

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import { Custom } from "codechain-sdk/lib/core/transaction/Custom";
 
 const RLP = require("rlp");
 
-const HANDLER_ID = 2;
+export const HANDLER_ID = 2;
 const TRANSFER_CCS_ACTION_ID = 1;
 const DELEGATE_CCS_ACTION_ID = 2;
 const REVOKE_ACTION_ID = 3;
@@ -77,6 +77,10 @@ export async function getDelegations(
             quantity: decodeU64(quantity)
         };
     });
+}
+
+function decodeUInt(buffer: Buffer): number {
+    return buffer.readUIntBE(0, buffer.length);
 }
 
 function decodeU64(buffer: Buffer): U64 {
@@ -148,4 +152,108 @@ export function createSelfNominateTransaction(
             metadata
         ])
     });
+}
+
+interface TransferCCS {
+    type: "transferCCS";
+    recipient: PlatformAddress;
+    quantity: U64;
+}
+
+interface DelegateCCS {
+    type: "delegateCCS";
+    delegatee: PlatformAddress;
+    quantity: U64;
+}
+
+interface Revoke {
+    type: "revoke";
+    delegatee: PlatformAddress;
+    quantity: U64;
+}
+
+interface SelfNominate {
+    type: "selfNominate";
+    deposit: U64;
+    metadata: Buffer;
+}
+
+type Action = TransferCCS | DelegateCCS | Revoke | SelfNominate;
+
+export function actionFromCustom(sdk: SDK, custom: Custom): Action | null {
+    const { handlerId, bytes } = custom as any;
+    if (!U64.ensure(handlerId).eq(HANDLER_ID)) {
+        return null;
+    }
+    if (!Buffer.isBuffer(bytes)) {
+        throw new Error("bytes should be a number");
+    }
+    return actionFromRLP(sdk, bytes);
+}
+
+export function actionFromRLP(sdk: SDK, rlp: Buffer): Action {
+    const decoded = RLP.decode(rlp);
+    if (
+        !Array.isArray(decoded) ||
+        decoded.length < 1 ||
+        !Buffer.isBuffer(decoded[0])
+    ) {
+        throw new Error(
+            "RLP of a stake action must be an array and it should have at least a tag as a first item"
+        );
+    }
+
+    switch (decodeUInt(decoded[0])) {
+        case TRANSFER_CCS_ACTION_ID:
+            if (decoded.length !== 3) {
+                throw new Error(
+                    "A length of a RLP list of a transferCCS action must be 3"
+                );
+            }
+            return {
+                type: "transferCCS",
+                recipient: decodePlatformAddress(sdk, decoded[1]),
+                quantity: decodeU64(decoded[2])
+            };
+        case DELEGATE_CCS_ACTION_ID:
+            if (decoded.length !== 3) {
+                throw new Error(
+                    "A length of a RLP list of a delegateCCS action must be 3"
+                );
+            }
+            return {
+                type: "delegateCCS",
+                delegatee: decodePlatformAddress(sdk, decoded[1]),
+                quantity: decodeU64(decoded[2])
+            };
+        case REVOKE_ACTION_ID:
+            if (decoded.length !== 3) {
+                throw new Error(
+                    "A length of a RLP list of a revoke action must be 3"
+                );
+            }
+            return {
+                type: "revoke",
+                delegatee: decodePlatformAddress(sdk, decoded[1]),
+                quantity: decodeU64(decoded[2])
+            };
+        case SELF_NOMINATE_ACTION_ID:
+            if (decoded.length !== 3) {
+                throw new Error(
+                    "A length of a RLP list of a selfNominate action must be 3"
+                );
+            }
+            if (!Buffer.isBuffer(decoded[2])) {
+                throw new Error(
+                    "The metadata field of a RLP encoded selfNominate action must be a string"
+                );
+            }
+            return {
+                type: "selfNominate",
+                deposit: decodeU64(decoded[1]),
+                metadata: decoded[2]
+            };
+        default:
+            throw new Error("Invalid tag for a stake action");
+    }
 }
