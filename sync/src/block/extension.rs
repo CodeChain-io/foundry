4ae657599ca26b490c12b59155a9c9150d66010f
Joonmo Yang <jmyang@codechain.io>
2019-11-14 19:09:51 +0900

    Rename total_score to seq in sync message
    When CodeChain is synchronized by snapshot sync, `total_score`
    doesn't mean the accumulated score from the genesis block.
    To handle this, the sync extension is updated to not rely on
    `total_score` for deciding whether a peer is leading or not, and
    use the value only as a monotonically increasing nonce.

--- sync/src/block/extension.rs
+++ sync/src/block/extension.rs
@@ -21,8 +21,8 @@ use std::time::Duration;
 
 use ccore::encoded::Header as EncodedHeader;
 use ccore::{
-    Block, BlockChainClient, BlockChainTrait, BlockId, BlockImportError, ChainNotify, Client, ImportBlock, ImportError,
-    UnverifiedTransaction,
+    Block, BlockChainClient, BlockChainTrait, BlockId, BlockImportError, BlockStatus, ChainNotify, Client, ImportBlock,
+    ImportError, UnverifiedTransaction,
 };
 use cnetwork::{Api, EventSender, NetworkExtension, NodeId};
 use cstate::FindActionHandler;
@@ -64,6 +64,7 @@ pub struct Extension {
     client: Arc<Client>,
     api: Box<dyn Api>,
     last_request: u64,
+    seq: u64,
 }
 
 impl Extension {
@@ -98,6 +99,7 @@ impl Extension {
             client,
             api,
             last_request: Default::default(),
+            seq: Default::default(),
         }
     }
 
@@ -113,13 +115,14 @@ impl Extension {
             id,
             Arc::new(
                 Message::Status {
-                    total_score: chain_info.best_proposal_score,
+                    seq: U256::from(self.seq),
                     best_hash: chain_info.best_proposal_block_hash,
                     genesis_hash: chain_info.genesis_hash,
                 }
                 .rlp_bytes(),
             ),
         );
+        self.seq += 1;
     }
 
     fn send_status_broadcast(&mut self) {
@@ -129,13 +132,14 @@ impl Extension {
                 id,
                 Arc::new(
                     Message::Status {
-                        total_score: chain_info.best_proposal_score,
+                        seq: U256::from(self.seq),
                         best_hash: chain_info.best_proposal_block_hash,
                         genesis_hash: chain_info.genesis_hash,
                     }
                     .rlp_bytes(),
                 ),
             );
+            self.seq += 1;
         }
     }
 
@@ -150,6 +154,14 @@ impl Extension {
     }
 
     fn send_body_request(&mut self, id: &NodeId) {
+        if let Some(downloader) = self.header_downloaders.get(&id) {
+            if self.client.block_status(&BlockId::Hash(downloader.best_hash())) == BlockStatus::InChain {
+                // Peer is lagging behind the local blockchain.
+                // We don't need to request block bodies to this peer
+                return
+            }
+        }
+
         self.check_sync_variable();
         if let Some(requests) = self.requests.get_mut(id) {
             let have_body_request = {
@@ -292,10 +304,10 @@ impl NetworkExtension<Event> for Extension {
         if let Ok(received_message) = Rlp::new(data).as_val() {
             match received_message {
                 Message::Status {
-                    total_score,
+                    seq,
                     best_hash,
                     genesis_hash,
-                } => self.on_peer_status(id, total_score, best_hash, genesis_hash),
+                } => self.on_peer_status(id, seq, best_hash, genesis_hash),
                 Message::Request(request_id, request) => self.on_peer_request(id, request_id, request),
                 Message::Response(request_id, response) => self.on_peer_response(id, request_id, response),
             }
@@ -307,7 +319,6 @@ impl NetworkExtension<Event> for Extension {
     fn on_timeout(&mut self, token: TimerToken) {
         match token {
             SYNC_TIMER_TOKEN => {
-                let best_proposal_score = self.client.chain_info().best_proposal_score;
                 let mut peer_ids: Vec<_> = self.header_downloaders.keys().cloned().collect();
                 peer_ids.shuffle(&mut thread_rng());
 
@@ -320,15 +331,7 @@ impl NetworkExtension<Event> for Extension {
                 }
 
                 for id in peer_ids {
-                    let peer_score = if let Some(peer) = self.header_downloaders.get(&id) {
-                        peer.total_score()
-                    } else {
-                        U256::zero()
-                    };
-
-                    if peer_score > best_proposal_score {
-                        self.send_body_request(&id);
-                    }
+                    self.send_body_request(&id);
                 }
             }
             SYNC_EXPIRE_TOKEN_BEGIN..=SYNC_EXPIRE_TOKEN_END => {
@@ -442,7 +445,7 @@ impl Extension {
 }
 
 impl Extension {
-    fn on_peer_status(&mut self, from: &NodeId, total_score: U256, best_hash: BlockHash, genesis_hash: BlockHash) {
+    fn on_peer_status(&mut self, from: &NodeId, seq: U256, best_hash: BlockHash, genesis_hash: BlockHash) {
         // Validity check
         if genesis_hash != self.client.chain_info().genesis_hash {
             cinfo!(SYNC, "Genesis hash mismatch with peer {}", from);
@@ -451,17 +454,17 @@ impl Extension {
 
         match self.header_downloaders.entry(*from) {
             Entry::Occupied(mut peer) => {
-                if !peer.get_mut().update(total_score, best_hash) {
+                if !peer.get_mut().update(seq, best_hash) {
                     // FIXME: It should be an error level if the consensus is PoW.
                     cdebug!(SYNC, "Peer #{} status updated but score is less than before", from);
                     return
                 }
             }
             Entry::Vacant(e) => {
-                e.insert(HeaderDownloader::new(self.client.clone(), total_score, best_hash));
+                e.insert(HeaderDownloader::new(self.client.clone(), seq, best_hash));
             }
         }
-        cinfo!(SYNC, "Peer #{} status update: total_score: {}, best_hash: {}", from, total_score, best_hash);
+        cinfo!(SYNC, "Peer #{} status update: seq: {}, best_hash: {}", from, seq, best_hash);
     }
 
     fn on_peer_request(&self, from: &NodeId, id: u64, request: RequestMessage) {
@@ -651,14 +654,12 @@ impl Extension {
 
     fn on_header_response(&mut self, from: &NodeId, headers: &[Header]) {
         ctrace!(SYNC, "Received header response from({}) with length({})", from, headers.len());
-        let (mut completed, pivot_score_changed) = if let Some(peer) = self.header_downloaders.get_mut(from) {
-            let before_pivot_score = peer.pivot_score();
+        let (mut completed, peer_is_caught_up) = if let Some(peer) = self.header_downloaders.get_mut(from) {
             let encoded: Vec<_> = headers.iter().map(|h| EncodedHeader::new(h.rlp_bytes().to_vec())).collect();
             peer.import_headers(&encoded);
-            let after_pivot_score = peer.pivot_score();
-            (peer.downloaded(), before_pivot_score != after_pivot_score)
+            (peer.downloaded(), peer.is_caught_up())
         } else {
-            (Vec::new(), false)
+            (Vec::new(), true)
         };
         completed.sort_unstable_by_key(EncodedHeader::number);
 
@@ -684,7 +685,7 @@ impl Extension {
             peer.mark_as_imported(exists);
             peer.create_request()
         });
-        if pivot_score_changed {
+        if !peer_is_caught_up {
             if let Some(request) = request {
                 self.send_header_request(from, request);
             }
@@ -724,20 +725,11 @@ impl Extension {
             }
         }
 
-        let total_score = self.client.chain_info().best_proposal_score;
         let mut peer_ids: Vec<_> = self.header_downloaders.keys().cloned().collect();
         peer_ids.shuffle(&mut thread_rng());
 
         for id in peer_ids {
-            let peer_score = if let Some(peer) = self.header_downloaders.get(&id) {
-                peer.total_score()
-            } else {
-                U256::zero()
-            };
-
-            if peer_score > total_score {
-                self.send_body_request(&id);
-            }
+            self.send_body_request(&id);
         }
     }
 }
