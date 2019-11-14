6b904c31691e22dd6b37f5b29369bb44f7e53d13
Joonmo Yang <jmyang@codechain.io>
2019-11-14 11:49:55 +0900

    Make the snapshot header importable without parent

--- core/src/client/client.rs
+++ core/src/client/client.rs
@@ -28,7 +28,7 @@ use cstate::{
 };
 use ctimer::{TimeoutHandler, TimerApi, TimerScheduleError, TimerToken};
 use ctypes::transaction::{AssetTransferInput, PartialHashing, ShardTransaction};
-use ctypes::{BlockHash, BlockNumber, CommonParams, ShardId, Tracker, TxHash};
+use ctypes::{BlockHash, BlockNumber, CommonParams, Header, ShardId, Tracker, TxHash};
 use cvm::{decode, execute, ChainTimeInfo, ScriptResult, VMConfig};
 use kvdb::{DBTransaction, KeyValueDB};
 use parking_lot::{Mutex, RwLock, RwLockReadGuard};
@@ -632,6 +632,15 @@ impl ImportBlock for Client {
             }
         }
         Ok(self.importer.header_queue.import(unverified)?)
+    }
+
+    fn import_bootstrap_header(&self, header: &Header) -> Result<BlockHash, BlockImportError> {
+        if self.block_chain().is_known_header(&header.hash()) {
+            return Err(BlockImportError::Import(ImportError::AlreadyInChain))
+        }
+        let import_lock = self.importer.import_lock.lock();
+        self.importer.import_bootstrap_header(header, self, &import_lock);
+        Ok(header.hash())
     }
 
     fn import_sealed_block(&self, block: &SealedBlock) -> ImportResult {
