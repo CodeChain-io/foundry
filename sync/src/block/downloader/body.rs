9743ec8683a88ba9f31efe4fdeadc6de1e334bde
Seulgi Kim <seulgi.kim@codechain.io>
2020-01-26 13:44:32 +0900

    Fix BodyDownloader to not import an invalid body
    Currently, BodyDownloader imports the body which should be empty, but
    the peer sends some body. This patch fixes it to not importing.

--- sync/src/block/downloader/body.rs
+++ sync/src/block/downloader/body.rs
@@ -53,13 +53,17 @@ impl BodyDownloader {
     }
 
     pub fn import_bodies(&mut self, hashes: Vec<BlockHash>, bodies: Vec<Vec<UnverifiedTransaction>>) {
+        let empty_targets: HashSet<_> = self.targets.iter().filter(|t| t.is_empty).map(|t| t.hash).collect();
         for (hash, body) in hashes.into_iter().zip(bodies) {
             if self.downloading.remove(&hash) {
                 if body.is_empty() {
-                    let target = self.targets.iter().find(|t| t.hash == hash).expect("Downloading target must exist");
-                    if !target.is_empty {
+                    if !empty_targets.contains(&hash) {
+                        cwarn!(SYNC, "Invalid body of {}. It should be not empty.", hash);
                         continue
                     }
+                } else if empty_targets.contains(&hash) {
+                    cwarn!(SYNC, "Invalid body of {}. It should be empty.", hash);
+                    continue
                 }
                 self.downloaded.insert(hash, body);
             }
