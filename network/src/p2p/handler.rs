cb04d7a69c663326f8b0736997bde8175deef079
Seulgi Kim <seulgi.kim@codechain.io>
2020-01-30 16:05:44 +0900

    Reduce the number of db transactions of PeerDb
    Currently, the address is saved in the db immediately. It increases the
    disk usage on the booting since there will many network update.
    
    This patch changes PeerDb to update disk per 10 times. It also makes the
    db store all peers in the same field. The constant "peer-list" is used
    for the key. And dedicated column for the peer list is removed. The
    addresses will be stored in the extra column.

--- network/src/p2p/handler.rs
+++ network/src/p2p/handler.rs
@@ -79,7 +79,7 @@ const RTT: Duration = Duration::from_secs(10); // T2
 const WAIT_SYNC: Duration = Duration::from_secs(30); // T3 >> T1 + RTT
 
 pub trait ManagingPeerdb: Send + Sync {
-    fn insert(&self, key: &SocketAddr);
+    fn insert(&self, key: SocketAddr);
     fn delete(&self, key: &SocketAddr);
 }
 
@@ -498,7 +498,7 @@ impl IoHandler<Message> for Handler {
             } => {
                 let mut inbound_connections = self.inbound_connections.write();
                 let target = connection.peer_addr();
-                self.peer_db.insert(&target);
+                self.peer_db.insert(*target);
                 if let Some(token) = self.inbound_tokens.lock().gen() {
                     let remote_node_id = connection.peer_addr().into();
                     assert_eq!(
