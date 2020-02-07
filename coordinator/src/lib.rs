630ac10ea91e4ad3cece1eb585e3a880adab2cb4
DoHyung Kim <dohyung.kim@seasonfive.co>
2020-02-07 19:30:41 +0900

    Add the draft design

---
+++ coordinator/src/lib.rs
@@ -0,0 +1,32 @@
+pub mod context;
+pub mod validator;
+
+/// The `Coordinator` encapsulates all the logic for a Foundry application.
+///
+/// It assembles modules and feeds them various events from the underlying
+/// consensus engine.
+pub struct Coordinator<C: context::Context> {
+    context: C,
+}
+
+impl<C: context::Context> validator::Validator for Coordinator<C> {}
+
+impl<C: context::Context> Coordinator<C> {}
+
+pub struct Builder<C: context::Context> {
+    context: C,
+}
+
+impl<C: context::Context> Builder<C> {
+    fn new<CTX: context::Context>(ctx: CTX) -> Builder<CTX> {
+        Builder {
+            context: ctx,
+        }
+    }
+
+    fn build(self) -> Coordinator<C> {
+        Coordinator {
+            context: self.context,
+        }
+    }
+}
