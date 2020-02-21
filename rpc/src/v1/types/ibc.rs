68862a442cee762538f9c03767e4224b1eda3a63
Junha Yang0 <junha@codechain.io>
2020-02-21 16:12:13 +0900

    Implement 3 basic RPCs for ICS 02 client

--- rpc/src/v1/types/ibc.rs
+++ rpc/src/v1/types/ibc.rs
@@ -13,3 +13,58 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use codechain_core::ibc::client_02::types::{ClientState as CoreClientState, ConsensusState as CoreConsensusState};
+use primitives::{Bytes, H256};
+use serde::Serialize;
+
+/// Many of RPC responses will be expressed with this
+/// Because of the nature of IBC, they commonly
+/// 1. Requires a block number for which proof stands
+/// 2. The data should be transparent:
+/// relayer must be able to open it and extract required infomation
+/// 3. Includes a cryptographical proof of that
+/// Note : proof may represents both that of presence and absence. It depends on option of data.
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct IBCQuery<T: Serialize> {
+    pub number: u64,
+    pub data: Option<T>,
+    pub proof: Bytes,
+}
+
+/// Client 02 related types
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ClientState {
+    /// Unpacked light_client::ClientState
+    pub number: u64,
+    pub next_validator_set_hash: H256,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ConsensusState {
+    pub validator_set_hash: H256,
+    /// Unpacked CommitmentRoot
+    pub state_root: H256,
+}
+
+impl ClientState {
+    pub fn from_core(state: &CoreClientState) -> Self {
+        ClientState {
+            number: state.raw.number,
+            next_validator_set_hash: state.raw.next_validator_set_hash,
+        }
+    }
+}
+
+impl ConsensusState {
+    pub fn from_core(state: &CoreConsensusState) -> Self {
+        ConsensusState {
+            validator_set_hash: state.validator_set_hash,
+            state_root: state.state_root.raw,
+        }
+    }
+}
