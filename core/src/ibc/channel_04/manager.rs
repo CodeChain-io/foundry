012b9743c644f494d2cc561906e73f68a65a7557
Junha Yang0 <junha@codechain.io>
2020-02-27 11:17:06 +0900

    Implement ICS04 chan_open_try()

--- core/src/ibc/channel_04/manager.rs
+++ core/src/ibc/channel_04/manager.rs
@@ -20,6 +20,8 @@ use crate::ibc;
 use crate::ibc::connection_03::path as connection_path;
 use crate::ibc::connection_03::types::{ConnectionEnd, ConnectionState};
 use crate::ibc::Identifier;
+use ibc::client_02::Manager as ClientManager;
+use primitives::Bytes;
 
 pub struct Manager<'a> {
     ctx: &'a mut dyn ibc::Context,
@@ -74,6 +76,107 @@ impl<'a> Manager<'a> {
         if kv_store.insert(&channel_path(DEFAULT_PORT, &channel_identifier), &rlp::encode(&channel)).is_some() {
             return Err("Channel exists".to_owned())
         }
+
+        let key = port05_generate();
+        assert!(kv_store
+            .insert(&channel_capability_path(DEFAULT_PORT, &channel_identifier), &rlp::encode(&key))
+            .is_none());
+
+        assert!(kv_store
+            .insert(
+                &next_sequence_send_path(DEFAULT_PORT, &channel_identifier),
+                &rlp::encode(&Sequence {
+                    raw: 1
+                })
+            )
+            .is_none());
+
+        assert!(kv_store
+            .insert(
+                &next_sequence_recv_path(DEFAULT_PORT, &channel_identifier),
+                &rlp::encode(&Sequence {
+                    raw: 1
+                })
+            )
+            .is_none());
+
+        Ok(key)
+    }
+
+    pub fn chan_open_try(
+        &mut self,
+        order: ChannelOrder,
+        connection: Identifier,
+        channel_identifier: Identifier,
+        counterparty_channel_identifier: Identifier,
+        version: String,
+        counterparty_version: String,
+        proof_init: Bytes,
+        proof_height: u64,
+    ) -> Result<Identifier, String> {
+        let kv_store = self.ctx.get_kv_store();
+
+        let previous = kv_store.get(&channel_path(&channel_identifier, DEFAULT_PORT));
+
+        // A trivial case is establishing a channel from scratch, so there won't be an existing one while responding counterparty's 'init' with 'try'.
+        // However, if it exists, we just check that our last trial ('init') was intended to make a same channel as the counterparty chain's 'init' did.
+        // And then overwrites it.
+        if let Some(x) = previous {
+            let channel_end: ChannelEnd = rlp::decode(&x).expect("Illformed connection end stored in the DB");
+            let expected = ChannelEnd {
+                state: ChannelState::INIT,
+                ordering: order,
+                counterparty_port_identifier: DEFAULT_PORT.to_string(),
+                counterparty_channel_identifier: counterparty_channel_identifier.clone(),
+                connection_hops: vec![connection.clone()],
+                version: version.clone(),
+            };
+            if channel_end != expected {
+                return Err("There already exists ChannelEnd on which open_try() can't be conducted.".to_owned())
+            }
+        }
+
+        let connection_end: ConnectionEnd = rlp::decode(
+            &kv_store.get(&connection_path(&connection)).ok_or_else(|| "Connection doesn't exist".to_owned())?,
+        )
+        .expect("Illformed connection end stored in the DB");
+
+        if connection_end.state != ConnectionState::OPEN {
+            return Err("Connection not opend".to_owned())
+        }
+
+        let expected = ChannelEnd {
+            state: ChannelState::INIT,
+            ordering: order,
+            counterparty_port_identifier: DEFAULT_PORT.to_string(),
+            counterparty_channel_identifier: channel_identifier.clone(),
+            // Note: the array should be reversed in the future where `connection` becomes an array.
+            connection_hops: vec![connection.clone()],
+            version: counterparty_version,
+        };
+
+        let client_manager = ClientManager::new(self.ctx);
+        client_manager.verify_channel_state(
+            &connection_end.client_identifier,
+            proof_height,
+            proof_init,
+            DEFAULT_PORT,
+            &counterparty_channel_identifier,
+            &expected,
+        )?;
+
+        let channel = ChannelEnd {
+            state: ChannelState::TRYOPEN,
+            ordering: order,
+            counterparty_port_identifier: DEFAULT_PORT.to_string(),
+            counterparty_channel_identifier,
+            connection_hops: vec![connection],
+            version,
+        };
+
+        let kv_store = self.ctx.get_kv_store_mut();
+
+        kv_store.insert(&channel_path(DEFAULT_PORT, &channel_identifier), &rlp::encode(&channel));
 
         let key = port05_generate();
         assert!(kv_store
