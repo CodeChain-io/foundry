0c5b0b63dc74aa7f4dc4a3d148ad9cd386416d09
Junha Yang0 <junha@codechain.io>
2020-02-28 12:07:03 +0900

    Implement all remaining state verifiers in ICS02

--- core/src/ibc/client_02/manager.rs
+++ core/src/ibc/client_02/manager.rs
@@ -14,13 +14,13 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use self::commitment_23::{verify_membership, CommitmentPathCounter, CommitmentProofCounter};
+use self::commitment_23::{verify_membership, verify_non_membership, CommitmentPathCounter, CommitmentProofCounter};
 use super::types::{ClientState, ConsensusState, Header};
 use super::*;
 use crate::consensus::light_client::ClientState as ChainClientState;
 use crate::ctypes::BlockNumber;
 use crate::ibc;
-use crate::ibc::channel_04::types::ChannelEnd;
+use crate::ibc::channel_04::types::{ChannelEnd, PacketCommitment, Sequence};
 use crate::ibc::connection_03::types::ConnectionEnd;
 use crate::ibc::IdentifierSlice;
 use crate::rlp::Encodable;
@@ -91,6 +91,10 @@ impl<'a> Manager<'a> {
     /// 2. They run on `Manager`, which has a state DB context.
     /// 3. They don't take a ClientState: each function retrieves it by itself.
     /// 4. They all take `id`, which indicates the counterparty chain.
+    /// 5. verify_packet_data()'s interface is slightly different from the spec. It take only
+    /// relevant data(packet data & timeout), and calculate the actual value to store by itself.
+    /// This can be seen as a same principle we've applied here,
+    /// "Encoding counterparty's state is always light client's responsiblity"
 
     fn verify_common_presence(
         &self,
@@ -115,6 +119,29 @@ impl<'a> Manager<'a> {
             },
             value,
         ) {
+            Ok(())
+        } else {
+            Err("Invalid proof".to_owned())
+        }
+    }
+
+    fn verify_common_absence(
+        &self,
+        id: IdentifierSlice,
+        proof_height: BlockNumber,
+        proof: Bytes,
+        path: String,
+    ) -> Result<(), String> {
+        let client_state = self.query(&id)?;
+        if client_state.raw.number < proof_height {
+            return Err("Invalid proof height".to_owned())
+        }
+        let consensus_state = self.query_consensus_state(&id, proof_height)?;
+        let proof_dec: CommitmentProofCounter = rlp::decode(&proof).map_err(|_| "Illformed proof")?;
+
+        if verify_non_membership(&consensus_state.state_root, &proof_dec, CommitmentPathCounter {
+            raw: path,
+        }) {
             Ok(())
         } else {
             Err("Invalid proof".to_owned())
@@ -148,5 +175,66 @@ impl<'a> Manager<'a> {
         let value_enc = rlp::encode(channel_end);
         self.verify_common_presence(id, proof_height, proof, path, value_enc)
             .map_err(|e| format!("{} : channel_state", e))
+    }
+
+    pub fn verify_packet_data(
+        &self,
+        id: IdentifierSlice,
+        proof_height: BlockNumber,
+        proof: Bytes,
+        port_identifier: IdentifierSlice,
+        channel_identifier: IdentifierSlice,
+        sequence: &Sequence,
+        packet_commitment: &PacketCommitment,
+    ) -> Result<(), String> {
+        let path = ibc::channel_04::packet_commitment_path(port_identifier, channel_identifier, sequence);
+        let value_enc = rlp::encode(&packet_commitment.hash());
+        self.verify_common_presence(id, proof_height, proof, path, value_enc)
+            .map_err(|e| format!("{} : packet_data", e))
+    }
+
+    pub fn verify_packet_acknowledgment(
+        &self,
+        id: IdentifierSlice,
+        proof_height: BlockNumber,
+        proof: Bytes,
+        port_identifier: IdentifierSlice,
+        channel_identifier: IdentifierSlice,
+        sequence: &Sequence,
+        acknowledgment: &[u8],
+    ) -> Result<(), String> {
+        let path = ibc::channel_04::packet_acknowledgement_path(port_identifier, channel_identifier, sequence);
+        let value_enc = rlp::encode(&acknowledgment);
+        self.verify_common_presence(id, proof_height, proof, path, value_enc)
+            .map_err(|e| format!("{} : packet_acknowledgment", e))
+    }
+
+    pub fn verify_packet_acknowledgment_absence(
+        &self,
+        id: IdentifierSlice,
+        proof_height: BlockNumber,
+        proof: Bytes,
+        port_identifier: IdentifierSlice,
+        channel_identifier: IdentifierSlice,
+        sequence: &Sequence,
+    ) -> Result<(), String> {
+        let path = ibc::channel_04::packet_acknowledgement_path(port_identifier, channel_identifier, sequence);
+        self.verify_common_absence(id, proof_height, proof, path)
+            .map_err(|e| format!("{} : packet_acknowledgment (absence)", e))
+    }
+
+    pub fn verify_next_sequence_recv(
+        &self,
+        id: IdentifierSlice,
+        proof_height: BlockNumber,
+        proof: Bytes,
+        port_identifier: IdentifierSlice,
+        channel_identifier: IdentifierSlice,
+        next_sequence_recv: &Sequence,
+    ) -> Result<(), String> {
+        let path = ibc::channel_04::next_sequence_recv_path(port_identifier, channel_identifier);
+        let value_enc = rlp::encode(next_sequence_recv);
+        self.verify_common_presence(id, proof_height, proof, path, value_enc)
+            .map_err(|e| format!("{} : next_sequence_recv", e))
     }
 }
