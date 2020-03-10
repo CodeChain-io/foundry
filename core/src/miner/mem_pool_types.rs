63fd7f37eb42eb16867accb14971ca28de31952c
Byeongjee Kang <byeongjee@codechain.io>
2020-03-10 18:12:18 +0900

    Replace CurrentQueue with TransactionPool
    Since transaction ordering will be done by applications,

--- core/src/miner/mem_pool_types.rs
+++ core/src/miner/mem_pool_types.rs
@@ -17,62 +17,10 @@
 use crate::transaction::VerifiedTransaction;
 use coordinator::TxOrigin;
 use ctypes::{BlockNumber, TxHash};
-use std::cmp::Ordering;
-use std::collections::BTreeSet;
+use std::collections::HashMap;
 
-/// Point in time when transaction was inserted.
+/// Point in time when transaction was inserted
 pub type PoolingInstant = BlockNumber;
-
-#[derive(Clone, Copy, Debug)]
-/// Light structure used to identify transaction and its order
-pub struct TransactionOrder {
-    /// Memory usage of this transaction.
-    /// Currently using the RLP byte length of the transaction as the mem usage.
-    pub mem_usage: usize,
-    /// Hash to identify associated transaction
-    pub hash: TxHash,
-    /// Incremental id assigned when transaction is inserted to the pool.
-    pub insertion_id: u64,
-    /// Origin of the transaction
-    pub origin: TxOrigin,
-}
-
-impl TransactionOrder {
-    pub fn for_transaction(item: &MemPoolItem) -> Self {
-        let rlp_bytes_len = rlp::encode(&item.tx).len();
-        ctrace!(MEM_POOL, "New tx with size {}", rlp_bytes_len);
-        Self {
-            mem_usage: rlp_bytes_len,
-            hash: item.hash(),
-            insertion_id: item.insertion_id,
-            origin: item.origin,
-        }
-    }
-}
-
-impl Eq for TransactionOrder {}
-impl PartialEq for TransactionOrder {
-    fn eq(&self, other: &TransactionOrder) -> bool {
-        self.cmp(other) == Ordering::Equal
-    }
-}
-impl PartialOrd for TransactionOrder {
-    fn partial_cmp(&self, other: &TransactionOrder) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for TransactionOrder {
-    fn cmp(&self, b: &TransactionOrder) -> Ordering {
-        // Local transactions should always have priority
-        if self.origin != b.origin {
-            return self.origin.cmp(&b.origin)
-        }
-
-        // Lastly compare insertion_id
-        self.insertion_id.cmp(&b.insertion_id)
-    }
-}
 
 /// Transaction item in the mem pool.
 #[derive(Clone, Eq, PartialEq, Debug, RlpEncodable)]
@@ -87,6 +35,9 @@ pub struct MemPoolItem {
     pub inserted_timestamp: u64,
     /// ID assigned upon insertion, should be unique.
     pub insertion_id: u64,
+    /// Memory usage of this transaction.
+    /// Currently using the RLP byte length of the transaction as the mem usage.
+    pub mem_usage: usize,
 }
 
 impl MemPoolItem {
@@ -97,12 +48,15 @@ impl MemPoolItem {
         inserted_timestamp: u64,
         insertion_id: u64,
     ) -> Self {
+        let mem_usage = rlp::encode(&tx).len();
+        ctrace!(MEM_POOL, "New tx with {} bytes", mem_usage);
         MemPoolItem {
             tx,
             origin,
             inserted_block_number,
             inserted_timestamp,
             insertion_id,
+            mem_usage,
         }
     }
 
@@ -112,47 +66,54 @@ impl MemPoolItem {
 }
 
 #[derive(Debug, PartialEq)]
-pub struct CurrentQueue {
-    /// Priority queue for transactions
-    pub queue: BTreeSet<TransactionOrder>,
-    /// Memory usage of the external transactions in the queue
+pub struct TransactionPool {
+    pub pool: HashMap<TxHash, MemPoolItem>,
+    /// Memory usage of the transactions in the queue
     pub mem_usage: usize,
     /// Count of the external transactions in the queue
     pub count: usize,
 }
 
-impl CurrentQueue {
+impl TransactionPool {
     pub fn new() -> Self {
         Self {
-            queue: BTreeSet::new(),
+            pool: Default::default(),
             mem_usage: 0,
             count: 0,
         }
     }
 
     pub fn clear(&mut self) {
-        self.queue.clear();
+        self.pool.clear();
         self.mem_usage = 0;
         self.count = 0;
     }
 
     pub fn len(&self) -> usize {
-        self.queue.len()
+        self.pool.len()
     }
 
-    pub fn insert(&mut self, order: TransactionOrder) {
-        self.queue.insert(order);
-        if !order.origin.is_local() {
-            self.mem_usage += order.mem_usage;
+    pub fn insert(&mut self, item: MemPoolItem) {
+        if !item.origin.is_local() {
+            self.mem_usage += item.mem_usage;
             self.count += 1;
         }
+        self.pool.insert(item.hash(), item);
     }
 
-    pub fn remove(&mut self, order: &TransactionOrder) {
-        assert!(self.queue.remove(order));
-        if !order.origin.is_local() {
-            self.mem_usage -= order.mem_usage;
-            self.count -= 1;
+    pub fn contains(&self, hash: &TxHash) -> bool {
+        self.pool.contains_key(hash)
+    }
+
+    pub fn remove(&mut self, hash: &TxHash) -> bool {
+        if let Some(item) = self.pool.remove(hash) {
+            if !item.origin.is_local() {
+                self.mem_usage -= item.mem_usage;
+                self.count -= 1;
+            }
+            true
+        } else {
+            false
         }
     }
 }
