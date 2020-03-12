b059dbab7f576619dfe53894ff8ecb1d1d4b4e01
Junha Yang0 <junha@codechain.io>
2020-03-12 14:07:52 +0900

    Implement a test environment of the basesandbox
    This includes one sample test.

---
+++ testmodule/README.md
@@ -0,0 +1,19 @@
+# Example modules for the test of Mold
+
+## How to add a new one (Linux/Mac binary)
+### Rust
+1. Write a code in `test-module/tests/`
+2. Update ``[[bin]]`` in `test-module/Cargo.toml`
+
+### C++
+Not supported yet
+
+## How to add a new one (Python)
+Not supported yet
+
+## How to add a new one (TypeScript)
+Not supported yet
+
+## How to add a new one (WASM Binary)
+Not supported yet
+
---
+++ testmodule/src/simple.rs
@@ -0,0 +1,12 @@
+extern crate codechain_basesandbox as cbasesandbox;
+use cbasesandbox::execution::executee;
+use cbasesandbox::ipc::Ipc;
+
+#[cfg(all(unix, target_arch = "x86_64"))]
+fn main() -> Result<(), String> {
+    let mut ctx = executee::start::<cbasesandbox::execution::IpcUnixDomainSocket>();
+    let r = ctx.ipc.recv();
+    assert_eq!(r, b"Hello?\0");
+    ctx.ipc.send(b"I'm here!\0");
+    Ok(())
+}
