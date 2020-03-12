15531fae0a8cb02eacc636bc4ab9682d5342d601
Seonpyo Kim <spkim@kodebox.io>
2020-03-12 21:37:54 +0900

    Construct the SubStorageAccess interface

--- coordinator/src/context.rs
+++ coordinator/src/context.rs
@@ -1,2 +1,20 @@
-/// A `Context` provides the interface against the system services such as
-pub trait Context {}
+pub use ctypes::StorageId;
+
+/// A `Context` provides the interface against the system services such as DB access,
+pub trait Context: SubStorageAccess {}
+
+pub type Key = dyn AsRef<[u8]>;
+pub type Value = Vec<u8>;
+pub trait SubStorageAccess {
+    fn get(&self, storage_id: StorageId, key: &Key) -> Option<Value>;
+    fn set(&mut self, storage_id: StorageId, key: &Key, value: Value);
+    fn has(&self, storage_id: StorageId, key: &Key) -> bool;
+    fn remove(&mut self, storage_id: StorageId, key: &Key);
+
+    /// Create a recoverable checkpoint of this state
+    fn create_checkpoint(&mut self);
+    /// Revert to the last checkpoint and discard it
+    fn revert_to_the_checkpoint(&mut self);
+    /// Merge last checkpoint with the previous
+    fn discard_checkpoint(&mut self);
+}
