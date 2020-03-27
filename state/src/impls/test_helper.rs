1422fd9a51e18534d81bf701c5cd79b0e11288e3
Seonpyo Kim <spkim@kodebox.io>
2020-03-27 21:00:35 +0900

    Refactor module_level test utilizing test_helper

--- state/src/impls/test_helper.rs
+++ state/src/impls/test_helper.rs
@@ -41,6 +41,61 @@ macro_rules! transaction {
     };
 }
 
+macro_rules! set_module_level_state {
+    // base cases
+    ($state:expr, [(key: $key:expr => datum: $datum:expr)]) => {
+        assert_eq!(Ok(()), $state.set_datum(&$key, $datum));
+    };
+    ($state:expr, [(key: $key:expr => datum_str: $datum_str:expr)]) => {
+        set_module_level_state!($state, [(key: $key => datum: String::from($datum_str).into_bytes())]);
+    };
+    ($state:expr, [(key: $key:expr => None)]) => {
+        assert_eq!(Ok(()), $state.remove(&$key));
+    };
+    // recursion
+    ($state:expr, [$head:tt, $($tail:tt),+ $(,)?]) => {
+        set_module_level_state!($state, [$head]);
+        set_module_level_state!($state, [$($tail),+]);
+    };
+}
+
+macro_rules! check_module_level_state {
+    // base cases
+    ($state:expr, [(key: $key:expr => datum: $datum:expr)]) => {
+        assert_eq!(Ok(true), $state.has_key(&$key));
+        assert_eq!(Some($datum), $state.get_datum(&$key).unwrap().map(|datum| datum.content()));
+    };
+    ($state:expr, [(key: $key:expr => datum_str: $datum_str:expr)]) => {
+        check_module_level_state!($state, [(key: $key => datum: String::from($datum_str).into_bytes())]);
+    };
+    ($state:expr, [(key: $key:expr => None)]) => {
+        assert_eq!(Ok(false), $state.has_key(&$key));
+        assert_eq!(None, $state.get_datum(&$key).unwrap());
+    };
+    ($state:expr, [(key: $key:expr => Some)]) => {
+        assert_eq!(Ok(true), $state.has_key(&$key));
+    };
+    // recursion
+    ($state:expr, [$head:tt, $($tail:tt),+ $(,)?]) => {
+        check_module_level_state!($state, [$head]);
+        check_module_level_state!($state, [$($tail),+]);
+    };
+}
+
+macro_rules! module_level {
+    ($state:expr, {check: $content:tt}) => {
+        check_module_level_state!($state, $content);
+    };
+    ($state:expr, {set: $content:tt}) => {
+        set_module_level_state!($state, $content);
+    };
+    //recursion
+    ($state:expr, {$opl:ident: $contentl:tt, $($opr:ident: $contentr:tt),+ $(,)?}) => {
+        module_level!($state, {$opl: $contentl});
+        module_level!($state, {$($opr: $contentr),+});
+    }
+}
+
 macro_rules! set_top_level_state {
     // base cases
     ($state:expr, [(account: $addr:expr => balance: $quantity:expr)]) => {
