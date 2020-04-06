36cfefaaf400484f38c6c3cb12e103daa88857cb
Junha Yang0 <junha@codechain.io>
2020-04-06 17:01:41 +0900

    Implement domain socket
    In MacOS, connect() is required even for the datagram communication, to
    use shutdown().

--- basesandbox/src/ipc.rs
+++ basesandbox/src/ipc.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+pub mod domain_socket;
+
 use once_cell::sync::OnceCell;
 use parking_lot::Mutex;
 
