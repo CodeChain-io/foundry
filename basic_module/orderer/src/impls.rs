605f5a3c4963d9ca155fb693cc0b3626741224be
Byeongjee Kang <byeongjee@codechain.io>
2020-04-10 14:15:16 +0900

    Add skeleton code for default Orderer module

---
+++ basic_module/orderer/src/impls.rs
@@ -0,0 +1,28 @@
+// Copyright 2020 Kodebox, Inc.
+// This file is part of CodeChain.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use super::core::Orderer;
+use super::types::{MetadataError, TxMetadata};
+
+#[allow(unused)]
+// SeqFeeOrderer will filter and order transactions using seqence and fee metadata
+pub struct SeqFeeOrderer {}
+
+impl Orderer for SeqFeeOrderer {
+    fn filter_and_order_transactions(&self, metadata_list: Vec<TxMetadata>) -> Result<Vec<usize>, MetadataError> {
+        unimplemented!()
+    }
+}
