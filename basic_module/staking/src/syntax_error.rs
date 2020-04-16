3ec46c2b0929e1b5b790726492e9946dd10c316f
SeonpyoKim <tomandjerry@postech.ac.kr>
2020-04-16 20:18:21 +0900

    Implement transaction and related types for the staking module

--- basic_module/staking/src/syntax_error.rs
+++ basic_module/staking/src/syntax_error.rs
@@ -14,7 +14,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use crate::types::{NetworkId, Signature};
+use crate::types::NetworkId;
+use fkey::Signature;
 use std::fmt;
 
 #[derive(Debug)]
---
+++ basic_module/staking/src/transactions.rs
@@ -0,0 +1,87 @@
+// Copyright 2020 Kodebox, Inc.
+// This file is part of CodeChain.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use crate::state::Params;
+use crate::types::{Approval, DepositQuantity, NetworkId, StakeQuantity};
+use ccrypto::blake256;
+use fkey::{verify, Ed25519Public as Public, Signature};
+use primitives::{Bytes, H256};
+
+pub struct SignedTransaction {
+    pub signature: Signature,
+    pub signer_public: Public,
+    pub tx: UserTransaction,
+}
+
+impl SignedTransaction {
+    pub fn verify(&self) -> bool {
+        let message = self.tx.hash();
+        verify(&self.signature, &message, &self.signer_public)
+    }
+}
+
+#[derive(Serialize)]
+pub struct UserTransaction {
+    /// Seq
+    pub seq: u64,
+    /// Quantity of CCC to be paid as a cost for distributing this transaction to the network.
+    pub fee: u64,
+    // Network id
+    pub network_id: NetworkId,
+    pub action: UserAction,
+}
+
+impl UserTransaction {
+    pub fn hash(&self) -> H256 {
+        let serialized = serde_cbor::to_vec(&self).unwrap();
+        blake256(serialized)
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Serialize)]
+pub enum UserAction {
+    TransferCCS {
+        receiver_public: Public,
+        quantity: StakeQuantity,
+    },
+    DelegateCCS {
+        delegatee_public: Public,
+        quantity: StakeQuantity,
+    },
+    Revoke {
+        delegatee_public: Public,
+        quantity: StakeQuantity,
+    },
+    Redelegate {
+        prev_delegatee: Public,
+        next_delegatee: Public,
+        quantity: StakeQuantity,
+    },
+    SelfNominate {
+        deposit: DepositQuantity,
+        metadata: Bytes,
+    },
+    ChangeParams {
+        metadata_seq: u64,
+        params: Params,
+        approvals: Vec<Approval>,
+    },
+    ReportDoubleVote {
+        message1: Bytes,
+        message2: Bytes,
+    },
+}
