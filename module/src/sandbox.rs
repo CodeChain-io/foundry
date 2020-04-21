733af11b8e58e33f70d256661cf2811898677c0f
Kim, DoHyung <dohyung.kim@seasonfive.co>
2020-04-21 15:00:48 +0900

    Revise module protocols (#336)
    * Remove dependency on crossbeam in favor of simple Receiver trait.
      Messages are anyway likely queued elsewhere to send to a module.
      We don't need to buffer them separately with crossbeam channels
    * Service objects are created at module creation time, and handles
      of some of them are exchanged at link creation time

--- module/src/sandbox.rs
+++ module/src/sandbox.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fmt::Debug;
 use std::path::Path;
 use std::sync::Arc;
@@ -6,11 +7,26 @@ use linkme::distributed_slice;
 use thiserror::Error;
 
 use crate::link::Linkable;
+use once_cell::sync;
 
 type Result<'a, T> = std::result::Result<T, Error<'a>>;
 
 #[distributed_slice]
 pub static SANDBOXERS: [fn() -> Arc<dyn Sandboxer>] = [..];
+
+/// Returns a `Sandboxer` with the given `id`.
+pub fn sandboxer(id: &str) -> Option<Arc<dyn Sandboxer>> {
+    static MAP: sync::Lazy<HashMap<&'static str, Arc<dyn Sandboxer>>> = sync::Lazy::new(|| {
+        SANDBOXERS
+            .iter()
+            .map(|new| {
+                let sandboxer = new();
+                (sandboxer.id(), sandboxer)
+            })
+            .collect()
+    });
+    MAP.get(id).map(Arc::clone)
+}
 
 /// An entity that can sandbox modules of types it supports.
 ///
@@ -22,13 +38,22 @@ pub trait Sandboxer {
     /// Returns a list of module types that can be loaded by this `Sandboxer`.
     fn supported_module_types(&self) -> &'static [&'static str];
 
-    /// Loads the module identified by the given `hash` into a [`Sandbox`].
+    /// Loads the module in the given `path` into a [`Sandbox`] and pass the given
+    /// `init` and `exports` to the module for initialization.
     ///
     /// The corresponding module must have been imported into the module repository
-    /// configured for the current Foundry host.
+    /// configured for the current Foundry host. That is why this method accepts a `path`
+    /// to identify a module.
+    ///
+    /// The `init` serves as configuration parameters for the module-wide initialization,
+    /// and must be CBOR encoded. The `exports` instruct how to instantiate an ordered list
+    /// of service objects to be exported via links. Each item in the `exports` designates
+    /// a call on a module's constructor function, where the first element is name
+    /// of a constructor function, and the second element is arguments to the constructor
+    /// function encoded in CBOR.
     ///
     /// [`Sandbox`]: ./trait.Sandbox.html
-    fn load(&self, hash: &dyn AsRef<Path>) -> Result<Arc<dyn Sandbox>>;
+    fn load(&self, path: &dyn AsRef<Path>, init: &[u8], exports: &[(&str, &[u8])]) -> Result<Arc<dyn Sandbox>>;
 }
 
 /// A sandbox instance hosting an instantiated module.
@@ -39,18 +64,18 @@ pub trait Sandbox: Linkable {
 
 #[derive(Debug, Error)]
 pub enum Error<'a> {
-    /// The module identified by the given `H256` is not in the module repository.
+    /// The module identified by the given `path` is not in the module repository.
     #[error("Could not find the specified module: {path:?}")]
     ModuleNotFound {
         path: &'a Path,
     },
 
-    /// The module identified by the given `H256` is not supported by the provider.
+    /// The module identified by the given `path` is not supported by the provider.
     #[error("The module is not supported: type '{ty:?}' at '{path:?}'")]
     UnsupportedModuleType {
         /// The identifier of the subject module.
         path: &'a Path,
-        /// The type of the subject module module.
+        /// The type of the subject module.
         ty: String,
     },
 }
