34cc8a8ae4b8e918a89ca47190412166cd9da454
Seulgi Kim <seulgi.kim@codechain.io>
2020-04-26 20:23:48 +0900

    Run the primitives test

--- test/src/primitives/test/address.test.ts
+++ test/src/primitives/test/address.test.ts
@@ -1,86 +1,85 @@
-import { H160, Address } from "../";
+import { H160, Address } from "../src";
+import "mocha";
+import { expect } from "chai";
 
 describe("Address", () => {
-    test("import", () => {
-        expect(typeof Address).toBe("function");
+    it("import", () => {
+        expect(typeof Address).equal("function");
     });
 
-    test("require", () => {
-        const obj = require("..");
-        expect(typeof obj.Address).toBe("function");
+    it("require", () => {
+        const obj = require("../src");
+        expect(typeof obj.Address).equal("function");
     });
 
-    test.skip("check", done => done.fail("not implemented"));
-    test.skip("ensure", done => done.fail("not implemented"));
-    test.skip("ensureAccount", done => done.fail("not implemented"));
-    test.skip("toString", done => done.fail("not implemented"));
+    it.skip("check", () => expect.fail("not implemented"));
+    it.skip("ensure", () => expect.fail("not implemented"));
+    it.skip("ensureAccount", () => expect.fail("not implemented"));
+    it.skip("toString", () => expect.fail("not implemented"));
 });
 
 describe("fromAccountId", () => {
     const accountId = new H160("7b5e0ee8644c6f585fc297364143280a45844502");
 
-    test("mainnet", () => {
+    it("mainnet", () => {
         const address = Address.fromAccountId(accountId, {
             networkId: "cc"
         });
-        expect(address.value).toBe(
+        expect(address.value).equal(
             "cccq9a4urhgv3xx7kzlc2tnvs2r9q9ytpz9qgs7q0a7"
         );
     });
 
-    test("testnet", () => {
+    it("testnet", () => {
         const address = Address.fromAccountId(accountId, {
             networkId: "tc"
         });
-        expect(address.value).toBe(
+        expect(address.value).equal(
             "tccq9a4urhgv3xx7kzlc2tnvs2r9q9ytpz9qgcejvw8"
         );
     });
 
-    test("Valid version", () => {
+    it("Valid version", () => {
         expect(() => {
             Address.fromAccountId(accountId, {
                 networkId: "tc",
                 version: 1
             });
-        }).not.toThrow();
+        }).not.throw();
     });
 
-    test("Invalid version", done => {
+    it("Invalid version", () => {
         try {
             Address.fromAccountId(accountId, {
                 networkId: "tc",
                 version: 99
             });
-            done.fail();
+            expect.fail();
         } catch (e) {
-            expect(e.toString()).toContain("version");
-            done();
+            expect(e.toString()).contains("version");
         }
     });
 
-    test("Invalid networkId", done => {
+    it("Invalid networkId", () => {
         try {
             Address.fromAccountId(accountId, {
                 version: 1,
                 networkId: "x"
             });
-            done.fail();
+            expect.fail();
         } catch (e) {
-            expect(e.toString()).toContain("networkId");
-            expect(e.toString()).toContain("x");
-            done();
+            expect(e.toString()).contains("networkId");
+            expect(e.toString()).contains("x");
         }
     });
 
-    test("Invalid accountId", done => {
+    it("Invalid accountId", () => {
         try {
             Address.fromAccountId("xxx", { networkId: "tc" });
-            done.fail();
+            expect.fail();
         } catch (e) {
-            expect(e.toString()).toContain("accountId");
-            expect(e.toString()).toContain("xxx");
-            done();
+            expect(e.toString()).contains("accountId");
+            expect(e.toString()).contains("xxx");
         }
     });
 });
@@ -90,25 +89,24 @@ describe("fromString", () => {
     const mainnetAddress = "cccq9a4urhgv3xx7kzlc2tnvs2r9q9ytpz9qgs7q0a7";
     const testnetAddress = "tccq9a4urhgv3xx7kzlc2tnvs2r9q9ytpz9qgcejvw8";
 
-    test("mainnet", () => {
+    it("mainnet", () => {
         const address = Address.fromString(mainnetAddress);
-        expect(address.accountId).toEqual(new H160(accountId));
+        expect(address.accountId.value).equal(new H160(accountId).value);
     });
 
-    test("testnet", () => {
+    it("testnet", () => {
         const address = Address.fromString(testnetAddress);
-        expect(address.accountId).toEqual(new H160(accountId));
+        expect(address.accountId.value).equal(new H160(accountId).value);
     });
 
-    test("Invalid checksum", done => {
+    it("Invalid checksum", () => {
         const invalidChecksumAddress =
             "cccqpa4urhgv3xx7kzlc2tnvs2r9q9ytpz9qgqqqqqq";
         try {
             Address.fromString(invalidChecksumAddress);
-            done.fail();
+            expect.fail();
         } catch (e) {
-            expect(e.toString()).toContain("checksum");
-            done();
+            expect(e.toString()).contains("checksum");
         }
     });
 });
@@ -118,29 +116,28 @@ describe("fromPublic", () => {
         "d7a6d266837c1c591383b90d835068b9ed58dd3bcebd6e285911f58e40ce413c";
     const accountId = "837cfc9c54fd1cd83970e0493d54d3a579aba06c";
 
-    test("mainnet", () => {
+    it("mainnet", () => {
         const address = Address.fromPublic(pubkey, { networkId: "cc" });
-        expect(address.toString()).toEqual(
+        expect(address.toString()).equal(
             "cccqxphelyu2n73ekpewrsyj0256wjhn2aqdsdp3qs3"
         );
-        expect(address.accountId).toEqual(new H160(accountId));
+        expect(address.accountId).deep.equal(new H160(accountId));
     });
 
-    test("testnet", () => {
+    it("testnet", () => {
         const address = Address.fromPublic(pubkey, { networkId: "tc" });
-        expect(address.toString()).toEqual(
+        expect(address.toString()).equal(
             "tccqxphelyu2n73ekpewrsyj0256wjhn2aqds9xrrrg"
         );
-        expect(address.accountId).toEqual(new H160(accountId));
+        expect(address.accountId).deep.equal(new H160(accountId));
     });
 
-    test("Invalid public key", done => {
+    it("Invalid public key", () => {
         try {
             Address.fromPublic(pubkey.slice(1), { networkId: "cc" });
-            done.fail();
+            expect.fail();
         } catch (e) {
-            expect(e.toString()).toContain("Invalid public key");
-            done();
+            expect(e.toString()).contains("Invalid public key");
         }
     });
 });
--- test/src/primitives/test/keyExchange.test.ts
+++ test/src/primitives/test/keyExchange.test.ts
@@ -1,4 +1,6 @@
-import { exchange, x25519GetPublicFromPrivate } from "..";
+import { exchange, x25519GetPublicFromPrivate } from "../src";
+import "mocha";
+import { expect } from "chai";
 
 describe("exchange", () => {
     // The testing oracle is generated from "libsodium-wrappers"
@@ -14,21 +16,21 @@ describe("exchange", () => {
     const shared_secret =
         "3e57e4716cbb9ffbcedce8c9b1516fe8b43dd6688020523b62e9142fc2cc5879";
 
-    test("Alice-side", () => {
-        expect(exchange(BobX25519Pk, AliceX25519Sk)).toEqual(shared_secret);
+    it("Alice-side", () => {
+        expect(exchange(BobX25519Pk, AliceX25519Sk)).equal(shared_secret);
     });
 
-    test("Bob-side", () => {
-        expect(exchange(AliceX25519Pk, BobX25519Sk)).toEqual(shared_secret);
+    it("Bob-side", () => {
+        expect(exchange(AliceX25519Pk, BobX25519Sk)).equal(shared_secret);
     });
 });
 
-test("x25519GetPublicFromPrivate", () => {
+it("x25519GetPublicFromPrivate", () => {
     // The testing oracle is generated from "libsodium-wrappers"
     const x25519Sk =
         "49023b9f7f997d3c8f1763f762a2f79d3d0fc5d4204ee25bf05ef57ca190d91b";
     const x25519Pk =
         "687d59b5c23877b6e822e3459d5a9a45e801a6a3752cdada2dfa7c76eaadb36f";
 
-    expect(x25519GetPublicFromPrivate(x25519Sk)).toEqual(x25519Pk);
+    expect(x25519GetPublicFromPrivate(x25519Sk)).equal(x25519Pk);
 });
