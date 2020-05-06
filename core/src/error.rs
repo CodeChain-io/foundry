5669550cb81a424295168aebcc787730e85c1a7c
Byeongjee Kang <byeongjee@codechain.io>
2020-05-06 18:53:23 +0900

    Remove an unnecessary error type
    Since we removed unnecessary initialization of genesis header extra
    data (c13c5ef899d3062cd718899b07901b4d0dfa72f5), we don't need this
    error type anymore.

--- core/src/error.rs
+++ core/src/error.rs
@@ -109,7 +109,6 @@ pub enum BlockError {
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum SchemeError {
-    InvalidCommonParams,
     InvalidState,
 }
 
@@ -117,7 +116,6 @@ impl fmt::Display for SchemeError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         use self::SchemeError::*;
         let msg: String = match self {
-            InvalidCommonParams => "Common params are not matched with gensis block".into(),
             InvalidState => "Genesis state is not same with spec".into(),
         };
         f.write_fmt(format_args!("Scheme file error ({})", msg))
