a6fb6cab6675f0b162e05ab3f12ebb5ab2060c70
Park Juhyung <jh@codechain.io>
2020-05-12 15:37:14 +0900

    Make onChainHeaderValid.test to use Tendermint
    This test checks Header verification.

--- test/src/e2e.long/onChainHeaderValid.test.ts
+++ test/src/e2e.long/onChainHeaderValid.test.ts
@@ -16,33 +16,81 @@
 
 import * as chai from "chai";
 import { expect } from "chai";
-import * as chaiAsPromised from "chai-as-promised";
 import "mocha";
+import * as RLP from "rlp";
 import { similar } from "../helper/chai-similar";
+import {
+    validator0Address,
+    validator1Address,
+    validator2Address,
+    validator3Address
+} from "../helper/constants";
 import { Mock } from "../helper/mock";
 import { Header } from "../helper/mock/cHeader";
+import { PromiseExpect } from "../helper/promise";
 import CodeChain from "../helper/spawn";
 import { Address, H256, U256 } from "../primitives/src";
 
 chai.use(similar);
 
 describe("Test onChain header communication", async function() {
+    const promiseExpect = new PromiseExpect();
     let nodeA: CodeChain;
     let mock: Mock;
-    let soloGenesisBlock: Header;
-    let soloBlock1: Header;
-    let soloBlock2: Header;
+    let genesisHeader: Header;
+    let header1: Header;
+    let block1Validator: any;
+    let header2: Header;
+    let nodes: CodeChain[];
 
     before(async function() {
-        const node = new CodeChain({
-            argv: ["--force-sealing"]
+        this.timeout(60_000);
+
+        const validatorAddresses = [
+            validator0Address,
+            validator1Address,
+            validator2Address,
+            validator3Address
+        ];
+        nodes = validatorAddresses.map(address => {
+            return new CodeChain({
+                chain: `${__dirname}/../scheme/tendermint-int.json`,
+                argv: [
+                    "--engine-signer",
+                    address.toString(),
+                    "--password-path",
+                    "test/tendermint/password.json",
+                    "--force-sealing",
+                    "--no-discovery"
+                ],
+                additionalKeysPath: "tendermint/keys"
+            });
         });
-        await node.start();
+        await Promise.all(nodes.map(node => node.start()));
 
-        const rpc = node.rpc;
+        await promiseExpect.shouldFulfill(
+            "connect",
+            Promise.all([
+                nodes[0].connect(nodes[1]),
+                nodes[0].connect(nodes[2]),
+                nodes[0].connect(nodes[3]),
+                nodes[1].connect(nodes[2]),
+                nodes[1].connect(nodes[3]),
+                nodes[2].connect(nodes[3])
+            ])
+        );
 
-        await rpc.devel!.startSealing();
-        await rpc.devel!.startSealing();
+        await promiseExpect.shouldFulfill(
+            "block generation",
+            Promise.all([
+                nodes[0].waitBlockNumber(2),
+                nodes[1].waitBlockNumber(2),
+                nodes[2].waitBlockNumber(2),
+                nodes[3].waitBlockNumber(2)
+            ])
+        );
+
+        const rpc = nodes[0].rpc;
 
         const genesisBlock = await rpc.chain.getBlockByNumber({
             blockNumber: 0
@@ -50,18 +98,41 @@ describe("Test onChain header communication", async function() {
         if (genesisBlock == null) {
             throw Error("Cannot get the genesis block");
         }
+        const genesisSeal = genesisBlock.seal.map(intArray =>
+            RLP.decode(new Buffer(intArray))
+        );
+
         const block1 = await rpc.chain.getBlockByNumber({ blockNumber: 1 });
         if (block1 == null) {
             throw Error("Cannot get the first block");
         }
+        const block1Seal = block1.seal.map(intArray =>
+            RLP.decode(new Buffer(intArray))
+        );
+        block1Validator = (
+            await rpc.call({ method: "chain_getValidatorSet" }, 1)
+        ).result;
+
         const block2 = await rpc.chain.getBlockByNumber({ blockNumber: 2 });
         if (block2 == null) {
             throw Error("Cannot get the second block");
         }
+        const block2Seal = block2.seal.map(intArray =>
+            RLP.decode(new Buffer(intArray))
+        );
 
-        await node.clean();
+        await promiseExpect.shouldFulfill(
+            "stop",
+            Promise.all([
+                nodes[0].clean(),
+                nodes[1].clean(),
+                nodes[2].clean(),
+                nodes[3].clean()
+            ])
+        );
+
         const author1PlatformAddr = Address.fromString(genesisBlock.author);
-        soloGenesisBlock = new Header(
+        genesisHeader = new Header(
             new H256(genesisBlock.parentHash),
             new U256(genesisBlock.timestamp),
             new U256(genesisBlock.number),
@@ -70,11 +141,11 @@ describe("Test onChain header communication", async function() {
             new H256(genesisBlock.transactionsRoot),
             new H256(genesisBlock.stateRoot),
             new H256(genesisBlock.nextValidatorSetHash),
-            genesisBlock.seal
+            genesisSeal
         );
         const author2PlatformAddr = Address.fromString(block1.author);
-        soloBlock1 = new Header(
-            soloGenesisBlock.hashing(),
+        header1 = new Header(
+            genesisHeader.hashing(),
             new U256(block1.timestamp),
             new U256(block1.number),
             author2PlatformAddr.pubkey,
@@ -82,11 +153,11 @@ describe("Test onChain header communication", async function() {
             new H256(block1.transactionsRoot),
             new H256(block1.stateRoot),
             new H256(block1.nextValidatorSetHash),
-            block1.seal
+            block1Seal
         );
         const author3PlatformAddr = Address.fromString(block2.author);
-        soloBlock2 = new Header(
-            soloBlock1.hashing(),
+        header2 = new Header(
+            header1.hashing(),
             new U256(block2.timestamp),
             new U256(block2.number),
             author3PlatformAddr.pubkey,
@@ -94,49 +165,46 @@ describe("Test onChain header communication", async function() {
             new H256(block2.transactionsRoot),
             new H256(block2.stateRoot),
             new H256(block2.nextValidatorSetHash),
-            block2.seal
+            block2Seal
         );
 
-        nodeA = new CodeChain();
+        nodeA = new CodeChain({
+            chain: `${__dirname}/../scheme/tendermint-int.json`
+        });
         await nodeA.start();
         mock = new Mock("0.0.0.0", nodeA.port, "tc");
-        await mock.establish();
+        await mock.establish(header2.hashing());
     });
 
     it("OnChain valid header propagation test", async function() {
-        // mock.setLog();
-        const rpc = nodeA.rpc;
-
-        // Genesis block
-        const header = soloGenesisBlock;
-
-        // Block 1
-        const header1 = soloBlock1;
-
-        // Block 2
-        const header2 = soloBlock2;
-
-        await mock.sendStatus(new U256(0), header2.hashing(), header.hashing());
-        await mock.waitHeaderRequest();
         const headerRequest = mock.getBlockHeaderRequest();
         expect(headerRequest!.startNumber).is.similarTo(new U256(0));
 
         await mock.sendBlockHeaderResponse([
-            header.toEncodeObject(),
-            header1.toEncodeObject(),
-            header2.toEncodeObject()
+            [genesisHeader.toEncodeObject(), []],
+            [header1.toEncodeObject(), []],
+            [header2.toEncodeObject(), validatorToEncodeObject(block1Validator)]
         ]);
         await mock.waitBodyRequest();
         const bodyRequest = mock.getBlockBodyRequest();
         expect(bodyRequest!.data[0]).is.similarTo(header1.hashing());
-        expect(bodyRequest!.data[1]).is.similarTo(header2.hashing());
     }).timeout(50_000);
 
     afterEach(async function() {
+        promiseExpect.checkFulfilled();
         if (this.currentTest!.state === "failed") {
             nodeA.keepLogs();
         }
         await mock.end();
         await nodeA.clean();
     });
+
+    function validatorToEncodeObject(validatorSet: any) {
+        const result = [];
+        for (const entry of validatorSet) {
+            result.push(entry.publicKey);
+            result.push(entry.delegation);
+        }
+        return [result];
+    }
 });
