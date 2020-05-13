6dcd5ca6f8bdb4a2e4b7e3b89f9057fd805b7fc2
SeonpyoKim <tomandjerry@postech.ac.kr>
2020-05-13 16:49:50 +0900

    Improve functionality of TransactionExecutionOutcome

--- coordinator/src/types.rs
+++ coordinator/src/types.rs
@@ -230,8 +230,15 @@ pub enum VerifiedCrime {
     },
 }
 
+#[derive(Default)]
 pub struct TransactionExecutionOutcome {
     pub events: Vec<Event>,
+}
+
+impl TransactionExecutionOutcome {
+    fn push_event(&mut self, event: Event) {
+        self.events.push(event);
+    }
 }
 
 pub type HeaderError = String;
