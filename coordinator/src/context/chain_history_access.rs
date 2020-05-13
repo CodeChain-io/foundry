c4ec9aa6fdfda01c2932dc36a98bc90825dd8ea5
SeungMin Lee <seungmin@codechain.io>
2020-05-13 15:14:05 +0900

    Add a method to the host side to transfer Header
    We added trait 'ChainHistoryAccess' to transfer Header type from the
    host side to modules. This change allows the modules to use the Header
    type.

--- coordinator/src/context/chain_history_access.rs
+++ coordinator/src/context/chain_history_access.rs
@@ -14,16 +14,9 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-mod chain_history_access;
-mod mem_pool_access;
-mod storage_access;
-mod sub_storage_access;
+use crate::header::Header;
+use ctypes::BlockId;
 
-pub use chain_history_access::ChainHistoryAccess;
-pub use mem_pool_access::MemPoolAccess;
-pub use storage_access::StorageAccess;
-pub use sub_storage_access::SubStorageAccess;
-
-/// A `Context` provides the interface against the system services such as moulde substorage access,
-/// mempool access
-pub trait Context: SubStorageAccess + MemPoolAccess {}
+pub trait ChainHistoryAccess {
+    fn get_block_header(&self, block_id: BlockId) -> Option<Header>;
+}
