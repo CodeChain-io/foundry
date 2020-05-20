f335fcf3637fc5613f07c8b5dad5f9df14169e05
Park Juhyung <jh@codechain.io>
2020-05-20 11:27:57 +0900

    Fix mock example code

--- test/src/helper/mock/example/send-tx.ts
+++ test/src/helper/mock/example/send-tx.ts
@@ -1,5 +1,5 @@
 import { Mock } from "..";
-import * as SDK from "../../../sdk";
+import { SDK } from "../../../sdk";
 
 async function sendTransaction() {
     const mock = new Mock("0.0.0.0", 3485, "tc");
@@ -7,20 +7,19 @@ async function sendTransaction() {
     await mock.establish();
 
     const sdk = new SDK({
-        server: process.env.CODECHAIN_RPC_HTTP || "http://localhost:8080",
         networkId: process.env.CODECHAIN_NETWORK_ID || "tc"
     });
     const ACCOUNT_SECRET =
         process.env.ACCOUNT_SECRET ||
         "9af28f6fd6a1170dbee2cb8c34abab0408e6d811d212cdcde23f72473eb0d97ad7a6d266837c1c591383b90d835068b9ed58dd3bcebd6e285911f58e40ce413c";
     const unsigned = sdk.core.createPayTransaction({
-        recipient: "tccqruq09sfgax77nj4gukjcuq69uzeyv0jcs7vzngg",
-        amount: 10000
+        recipient: "fys3db1kOrI_rXyaTx9U2_RP-SlNK1q0LRXxYeQGBI1av35drZQtc0",
+        quantity: 10000
     });
     const signed = unsigned.sign({
         secret: ACCOUNT_SECRET,
         fee: 10,
-        nonce: 0
+        seq: 0
     });
 
     await mock.sendEncodedTransaction([signed.toEncodeObject()]);
