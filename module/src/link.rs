bf87131c5168a5736da526c228d3ecbeb0d328ff
DoHyung Kim <dohyung.kim@seasonfive.co>
2020-05-29 11:38:48 +0900

    Add copyright notices to module related source files

--- module/src/link.rs
+++ module/src/link.rs
@@ -1,3 +1,19 @@
+// Copyright 2020 Kodebox, Inc.
+// This file is part of CodeChain.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 use std::collections::HashMap;
 use std::sync::Arc;
 
@@ -102,7 +118,7 @@ pub trait Port: CastFromSync {
 /// from another [`Linkable`].
 ///
 /// [`Linkable`]: ./trait.Linkable.html
-pub trait Receiver {
+pub trait Receiver: Send + Sync {
     /// Places the given message (`[u8]`) and returns immediately.
     /// The `message` is typed `Box<dyn AsRef<[u8]>>` to allow for zero copy sending
     /// as much as possible. The intention is to wrap various types as they are if they
