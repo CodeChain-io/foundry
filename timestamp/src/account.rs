cde7aa2666c1f6d684728a9c6d2b5dd85f8ab7fe
Junha Yang0 <junha@codechain.io>
2020-06-23 11:43:51 +0900

    Implement account module

---
+++ timestamp/src/account.rs
@@ -0,0 +1,83 @@
+// Copyright 2020 Kodebox, Inc.
+// This file is part of CodeChain.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pub use ckey::{Ed25519Private as Private, Ed25519Public as Public};
+use coordinator::context::SubStorageAccess;
+use coordinator::module::*;
+use coordinator::types::*;
+use parking_lot::RwLock;
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+
+pub struct Context {
+    storage: RwLock<Arc<dyn SubStorageAccess>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Error {
+    NoSuchAccount,
+    AccountExists,
+    InvalidKey,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Account {
+    seq: u64,
+}
+
+pub trait AccountManager: Send + Sync {
+    fn create_account(&self, public: &Public) -> Result<(), Error>;
+
+    fn get_sequence(&self, public: &Public) -> Result<u64, Error>;
+
+    fn increase_sequence(&self, public: &Public) -> Result<(), Error>;
+}
+
+impl AccountManager for Context {
+    fn create_account(&self, public: &Public) -> Result<(), Error> {
+        let account = Account {
+            seq: 0,
+        };
+        if self.storage.read().has(public) {
+            return Err(Error::AccountExists)
+        }
+        self.storage.read().set(public, serde_cbor::to_vec(&account).unwrap());
+        Ok(())
+    }
+
+    fn get_sequence(&self, public: &Public) -> Result<u64, Error> {
+        let account: Account =
+            serde_cbor::from_slice(&self.storage.read().get(public).ok_or_else(|| Error::NoSuchAccount)?)
+                .map_err(|_| Error::InvalidKey)?;
+        Ok(account.seq)
+    }
+
+    fn increase_sequence(&self, public: &Public) -> Result<(), Error> {
+        let mut account: Account =
+            serde_cbor::from_slice(&self.storage.read().get(public).ok_or_else(|| Error::NoSuchAccount)?)
+                .map_err(|_| Error::InvalidKey)?;
+        account.seq += 1;
+        self.storage.read().set(public, serde_cbor::to_vec(&account).unwrap());
+        Ok(())
+    }
+}
+
+impl BlockOpen for Context {
+    fn block_opened(&self, storage: Box<dyn SubStorageAccess>) -> Result<(), HeaderError> {
+        *self.storage.write() = Arc::from(storage);
+        Ok(())
+    }
+}
