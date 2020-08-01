f38c6e8f4f0b32385abb3852265fef7f66ac7254
Byeongjee Kang <byeongjee@codechain.io>
2020-08-01 22:30:51 +0900

    Replace MemPoolItem wth TransactionWithMetadata

--- core/src/miner/mem_pool_types.rs
+++ core/src/miner/mem_pool_types.rs
@@ -14,59 +14,13 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use coordinator::{Transaction, TxOrigin};
-use ctypes::{BlockNumber, TxHash};
+use coordinator::TransactionWithMetadata;
+use ctypes::TxHash;
 use std::collections::HashMap;
-
-/// Point in time when transaction was inserted
-pub type PoolingInstant = BlockNumber;
-
-/// Transaction item in the mem pool.
-#[derive(Clone, Eq, PartialEq, Debug, RlpEncodable)]
-pub struct MemPoolItem {
-    /// Transaction.
-    pub tx: Transaction,
-    /// Transaction origin.
-    pub origin: TxOrigin,
-    /// Insertion time
-    pub inserted_block_number: PoolingInstant,
-    /// Insertion timstamp
-    pub inserted_timestamp: u64,
-    /// ID assigned upon insertion, should be unique.
-    pub insertion_id: u64,
-    /// Memory usage of this transaction.
-    /// Currently using the RLP byte length of the transaction as the mem usage.
-    pub mem_usage: usize,
-}
-
-impl MemPoolItem {
-    pub fn new(
-        tx: Transaction,
-        origin: TxOrigin,
-        inserted_block_number: PoolingInstant,
-        inserted_timestamp: u64,
-        insertion_id: u64,
-    ) -> Self {
-        let mem_usage = rlp::encode(&tx).len();
-        ctrace!(MEM_POOL, "New tx with {} bytes", mem_usage);
-        MemPoolItem {
-            tx,
-            origin,
-            inserted_block_number,
-            inserted_timestamp,
-            insertion_id,
-            mem_usage,
-        }
-    }
-
-    pub fn hash(&self) -> TxHash {
-        self.tx.hash()
-    }
-}
 
 #[derive(Debug, PartialEq)]
 pub struct TransactionPool {
-    pub pool: HashMap<TxHash, MemPoolItem>,
+    pub pool: HashMap<TxHash, TransactionWithMetadata>,
     /// Memory usage of the transactions in the queue
     pub mem_usage: usize,
     /// Count of the external transactions in the queue
@@ -92,9 +46,9 @@ impl TransactionPool {
         self.pool.len()
     }
 
-    pub fn insert(&mut self, item: MemPoolItem) {
+    pub fn insert(&mut self, item: TransactionWithMetadata) {
         if !item.origin.is_local() {
-            self.mem_usage += item.mem_usage;
+            self.mem_usage += item.size();
             self.count += 1;
         }
         self.pool.insert(item.hash(), item);
@@ -107,7 +61,7 @@ impl TransactionPool {
     pub fn remove(&mut self, hash: &TxHash) -> bool {
         if let Some(item) = self.pool.remove(hash) {
             if !item.origin.is_local() {
-                self.mem_usage -= item.mem_usage;
+                self.mem_usage -= item.size();
                 self.count -= 1;
             }
             true
