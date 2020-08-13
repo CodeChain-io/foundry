99a437198aecaf306f9419be54466524d3b4b5c8
Junha Yang <junha@codechain.io>
2020-08-13 17:06:03 +0900

    Add a type for transaction sequence

--- timestamp/src/stamp.rs
+++ timestamp/src/stamp.rs
@@ -159,7 +159,7 @@ struct GetAccountAndSeq;
 impl Service for GetAccountAndSeq {}
 
 impl crate::sorting::GetAccountAndSeq for GetAccountAndSeq {
-    fn get_account_and_seq(&self, tx: &Transaction) -> Result<(Public, u64), ()> {
+    fn get_account_and_seq(&self, tx: &Transaction) -> Result<(Public, TxSeq), ()> {
         assert_eq!(tx.tx_type(), "stamp");
         let tx: OwnTransaction = serde_cbor::from_slice(&tx.body()).map_err(|_| ())?;
         Ok((tx.signer_public, tx.tx.seq))
