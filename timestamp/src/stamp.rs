388237f47a295f8338680124cfb15759be22120f
Junha Yang <junha@codechain.io>
2020-08-14 14:16:55 +0900

    Fix various naming in modules

--- timestamp/src/stamp.rs
+++ timestamp/src/stamp.rs
@@ -179,19 +179,16 @@ impl UserModule for Module {
 
     fn prepare_service_to_export(&mut self, ctor_name: &str, ctor_arg: &[u8]) -> Skeleton {
         match ctor_name {
-            "tx_owner" => {
-                let arg: String = serde_cbor::from_slice(ctor_arg).unwrap();
-                assert_eq!(arg, "unused");
+            "tx-owner" => {
+                assert_empty_arg(ctor_arg).unwrap();
                 Skeleton::new(Arc::clone(&self.ctx) as Arc<RwLock<dyn TxOwner>>)
             }
-            "init_genesis" => {
-                let arg: String = serde_cbor::from_slice(ctor_arg).unwrap();
-                assert_eq!(arg, "unused");
+            "init-genesis" => {
+                assert_empty_arg(ctor_arg).unwrap();
                 Skeleton::new(Arc::clone(&self.ctx) as Arc<RwLock<dyn InitGenesis>>)
             }
-            "get_account_and_seq" => {
-                let arg: String = serde_cbor::from_slice(ctor_arg).unwrap();
-                assert_eq!(arg, "unused");
+            "get-account-and-seq" => {
+                assert_empty_arg(ctor_arg).unwrap();
                 Skeleton::new(Box::new(GetAccountAndSeq) as Box<dyn crate::sorting::GetAccountAndSeq>)
             }
             _ => panic!("Unsupported ctor_name in prepare_service_to_export() : {}", ctor_name),
@@ -200,10 +197,10 @@ impl UserModule for Module {
 
     fn import_service(&mut self, rto_context: &RtoContext, name: &str, handle: HandleToExchange) {
         match name {
-            "account_manager" => {
+            "account-manager" => {
                 self.ctx.write().account.replace(import_service_from_handle(rto_context, handle));
             }
-            "token_manager" => {
+            "token-manager" => {
                 self.ctx.write().token.replace(import_service_from_handle(rto_context, handle));
             }
             _ => panic!("Invalid name in import_service()"),
