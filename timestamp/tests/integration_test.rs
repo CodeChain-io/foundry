99a19d2b26d4b611a8a3940f32b421213fcfc5e7
Junha Yang <junha@codechain.io>
2020-09-03 14:07:58 +0900

    Setup MODULE_INITS for timestamp modules

--- timestamp/tests/integration_test.rs
+++ timestamp/tests/integration_test.rs
@@ -17,3 +17,53 @@
 extern crate codechain_module as cmodule;
 extern crate codechain_timestamp as timestamp;
 extern crate foundry_process_sandbox as fproc_sndbx;
+
+mod timestamp_setup {
+    use super::*;
+    use codechain_module::impls::process::{ExecutionScheme, SingleProcess};
+    use codechain_module::MODULE_INITS;
+    use foundry_module_rt::start;
+    use foundry_process_sandbox::execution::executor::add_function_pool;
+    use linkme::distributed_slice;
+    use std::sync::Arc;
+
+    #[distributed_slice(MODULE_INITS)]
+    fn account() {
+        add_function_pool(
+            "a010000000012345678901234567890123456789012345678901234567890123".to_owned(),
+            Arc::new(start::<<SingleProcess as ExecutionScheme>::Ipc, timestamp::account::Module>),
+        );
+    }
+
+    #[distributed_slice(MODULE_INITS)]
+    fn staking() {
+        add_function_pool(
+            "a020000000012345678901234567890123456789012345678901234567890123".to_owned(),
+            Arc::new(start::<<SingleProcess as ExecutionScheme>::Ipc, timestamp::staking::Module>),
+        );
+    }
+
+    #[distributed_slice(MODULE_INITS)]
+    fn stamp() {
+        add_function_pool(
+            "a030000000012345678901234567890123456789012345678901234567890123".to_owned(),
+            Arc::new(start::<<SingleProcess as ExecutionScheme>::Ipc, timestamp::stamp::Module>),
+        );
+    }
+
+    #[distributed_slice(MODULE_INITS)]
+    fn token() {
+        add_function_pool(
+            "a040000000012345678901234567890123456789012345678901234567890123".to_owned(),
+            Arc::new(start::<<SingleProcess as ExecutionScheme>::Ipc, timestamp::token::Module>),
+        );
+    }
+
+    #[distributed_slice(MODULE_INITS)]
+    fn sorting() {
+        add_function_pool(
+            "a050000000012345678901234567890123456789012345678901234567890123".to_owned(),
+            Arc::new(start::<<SingleProcess as ExecutionScheme>::Ipc, timestamp::sorting::Module>),
+        );
+    }
+}
