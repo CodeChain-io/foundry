9b7acc1ebad82f61d4c9ae36e1764f2c08736eac
Park Juhyung <jh@codechain.io>
2020-09-16 17:06:02 +0900

    Refactor actix handlers to use Extractor

--- graphql/src/lib.rs
+++ graphql/src/lib.rs
@@ -18,18 +18,18 @@ mod handler;
 
 use actix_web::{
     dev::Server,
-    error::{ErrorBadRequest, ErrorMethodNotAllowed, ErrorNotFound, ErrorUnsupportedMediaType},
-    http::{header::CONTENT_TYPE, Method},
+    error::{ErrorBadRequest, ErrorNotFound},
     web,
-    web::{Payload, Query, ServiceConfig},
-    App, FromRequest, HttpRequest, HttpResponse, HttpServer, Result,
+    web::ServiceConfig,
+    App, Error, FromRequest, HttpRequest, HttpResponse, HttpServer, Result,
 };
 use coordinator::module::{HandleGraphQlRequest, SessionId};
-use futures::StreamExt;
+use futures::Future;
 pub use handler::handle_gql_query;
+use serde::Deserialize;
 use std::collections::HashMap;
 use std::net::SocketAddr;
-use std::sync::Arc;
+use std::{pin::Pin, sync::Arc};
 
 // TODO: replace with the real Client.
 pub struct Client {}
@@ -60,90 +60,39 @@ impl ServerData {
     }
 }
 
-async fn handle_request(
-    request: HttpRequest,
-    payload: Payload,
-    path: web::Path<String>,
-    server_data: web::Data<Arc<ServerData>>,
-) -> Result<HttpResponse> {
-    let module_name = path.into_inner();
-    if let Some(GraphQlRequestHandler {
-        session_needed,
-        handler,
-    }) = server_data.graphql_handlers.get(&module_name)
-    {
-        let session = if *session_needed {
-            let height = None;
-            server_data.client.allocate_session(height)
-        } else {
-            0
-        };
-
-        match *request.method() {
-            Method::POST => handle_post(request, payload, &**handler, session).await,
-            Method::GET => handle_get(request, payload, &**handler, session).await,
-            _ => Err(ErrorMethodNotAllowed("GraphQL requests can only be sent with GET or POST")),
-        }
-    } else {
-        Err(ErrorNotFound(format!("Module not found: {}", module_name)))
-    }
+#[derive(Deserialize)]
+struct GraphQlArgs {
+    query: String,
+    variables: String,
 }
 
-const MAX_SIZE: usize = 262_144; // max payload size is 256k
+async fn handle_post(session_and_handler: SessionAndHandler, args: web::Json<GraphQlArgs>) -> Result<HttpResponse> {
+    let SessionAndHandler {
+        session,
+        handler,
+    } = session_and_handler;
+    let query = &args.query;
+    let variables = &args.variables;
 
-async fn handle_post(
-    request: HttpRequest,
-    mut payload: Payload,
-    handler: &dyn HandleGraphQlRequest,
-    session: SessionId,
-) -> Result<HttpResponse> {
-    let content_type_header = request.headers().get(CONTENT_TYPE).and_then(|hv| hv.to_str().ok());
-    let query: HashMap<String, String> = match content_type_header {
-        Some("application/json") | Some("application/graphql") => {
-            let mut body = web::BytesMut::new();
-            while let Some(chunk) = payload.next().await {
-                let chunk = chunk?;
-                // limit max size of in-memory payload
-                if (body.len() + chunk.len()) > MAX_SIZE {
-                    return Err(ErrorBadRequest("overflow"))
-                }
-                body.extend_from_slice(&chunk);
-            }
-            Ok(serde_json::from_slice(body.as_ref()).unwrap())
-        }
-        _ => Err(ErrorUnsupportedMediaType(
-            "GraphQL requests should have content type `application/json` or `application/graphql`",
-        )),
-    }?;
-    let graphql_query = query.get("query").ok_or_else(HttpResponse::BadRequest)?;
-    let variables = query.get("variables").ok_or_else(HttpResponse::BadRequest)?;
-
-    let graphql_response = handler.execute(session, graphql_query, variables);
+    let graphql_response = handler.execute(session, query, variables);
     Ok(HttpResponse::Ok().content_type("application/json").body(graphql_response))
 }
 
-async fn handle_get(
-    request: HttpRequest,
-    payload: Payload,
-    handler: &dyn HandleGraphQlRequest,
-    session: SessionId,
-) -> Result<HttpResponse> {
-    let mut payload = payload.into_inner();
-    let query: Query<HashMap<String, String>> =
-        Query::from_request(&request, &mut payload).await.map_err(|_| HttpResponse::BadRequest())?;
-    let query = query.into_inner();
-    let graphql_query = query.get("query").ok_or_else(HttpResponse::BadRequest)?;
-    let variables = query.get("variables").ok_or_else(HttpResponse::BadRequest)?;
+async fn handle_get(session_and_handler: SessionAndHandler, args: web::Query<GraphQlArgs>) -> Result<HttpResponse> {
+    let SessionAndHandler {
+        session,
+        handler,
+    } = session_and_handler;
+    let query = &args.query;
+    let variables = &args.variables;
 
-    let graphql_response = handler.execute(session, graphql_query, variables);
+    let graphql_response = handler.execute(session, query, variables);
     Ok(HttpResponse::Ok().content_type("application/json").body(graphql_response))
 }
 
 pub fn app_configure(config: &mut ServiceConfig, server_data: Arc<ServerData>) {
     config.data(Arc::clone(&server_data)).service(
-        web::resource("/{module_name}/graphql")
-            .route(web::post().to(handle_request))
-            .route(web::get().to(handle_request)),
+        web::resource("/{module_name}/graphql").route(web::post().to(handle_post)).route(web::get().to(handle_get)),
     );
 }
 
@@ -153,4 +102,43 @@ pub fn run_server(server_data: ServerData, addr: SocketAddr) -> Result<Server> {
         App::new().configure(|config: &mut ServiceConfig| app_configure(config, Arc::clone(&server_data)))
     });
     Ok(server.bind(addr)?.run())
+}
+
+struct SessionAndHandler {
+    pub session: SessionId,
+    pub handler: Arc<dyn HandleGraphQlRequest>,
+}
+
+impl FromRequest for SessionAndHandler {
+    type Error = Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self, Self::Error>>>>;
+    type Config = ();
+
+    fn from_request(req: &HttpRequest, _payload: &mut actix_http::Payload) -> Self::Future {
+        let module_name = req.match_info().get("module_name").map(|string| string.to_owned());
+        let server_data = req.app_data::<web::Data<Arc<ServerData>>>().unwrap().clone();
+        Box::pin(async move {
+            let module_name = module_name.ok_or_else(|| ErrorBadRequest("module_name not found"))?;
+
+            if let Some(GraphQlRequestHandler {
+                session_needed,
+                handler,
+            }) = server_data.graphql_handlers.get(&module_name)
+            {
+                let session = if *session_needed {
+                    let height = None;
+                    server_data.client.allocate_session(height)
+                } else {
+                    0
+                };
+
+                Ok(SessionAndHandler {
+                    session,
+                    handler: handler.clone(),
+                })
+            } else {
+                Err(ErrorNotFound(format!("Module not found: {}", module_name)))
+            }
+        })
+    }
 }
