e47adc64549cfdd7d72bc6c5af03d6fe371ff4f4
Park Juhyung <jh@codechain.io>
2020-10-06 18:15:24 +0900

    Remove unused code after removing stake account and actions

--- state/src/stake/mod.rs
+++ state/src/stake/mod.rs
@@ -14,24 +14,16 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use super::TopStateView;
 use crate::{StateResult, TopLevelState};
 use ccrypto::blake256;
 use ckey::Ed25519Public as Public;
 use ctypes::errors::SyntaxError;
 use primitives::H256;
 use rlp::{Encodable, RlpStream};
-use std::convert::From;
 
 pub trait DoubleVoteHandler: Send + Sync {
     fn execute(&self, message1: &[u8], state: &mut TopLevelState, fee_payer: &Public) -> StateResult<()>;
     fn verify(&self, message1: &[u8], message2: &[u8]) -> Result<(), SyntaxError>;
-}
-
-pub fn query(key_fragment: &[u8], state: &TopLevelState) -> StateResult<Option<Vec<u8>>> {
-    let key = StakeKeyBuilder::key_from_fragment(key_fragment);
-    let some_action_data = state.action_data(&key)?.map(Vec::from);
-    Ok(some_action_data)
 }
 
 pub struct StakeKeyBuilder {
@@ -45,12 +37,6 @@ impl StakeKeyBuilder {
         StakeKeyBuilder {
             rlp,
         }
-    }
-
-    pub fn key_from_fragment(key_fragment: &[u8]) -> H256 {
-        let mut builder = Self::prepare();
-        builder.rlp.append_raw(&key_fragment, 1);
-        builder.into_key()
     }
 
     pub fn new(fragment_length: usize) -> StakeKeyBuilder {
@@ -68,20 +54,5 @@ impl StakeKeyBuilder {
 
     pub fn into_key(self) -> H256 {
         blake256(self.rlp.as_raw())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn action_data_key_builder_raw_fragment_and_list_are_same() {
-        let key1 = StakeKeyBuilder::new(3).append(&"key").append(&"fragment").append(&"has trailing list").into_key();
-
-        let mut rlp = RlpStream::new_list(3);
-        rlp.append(&"key").append(&"fragment").append(&"has trailing list");
-        let key2 = StakeKeyBuilder::key_from_fragment(rlp.as_raw());
-        assert_eq!(key1, key2);
     }
 }
