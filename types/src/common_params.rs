0bbc63fedd1fe353344e10801bcda37be7929ba0
Park Juhyung <jh@codechain.io>
2020-10-08 16:33:17 +0900

    Remove unused era
    Era was introduced to upgrade a chain. It only has config value and
    not used in Foundry now.

--- types/src/common_params.rs
+++ types/src/common_params.rs
@@ -27,8 +27,6 @@ pub struct CommonParams {
     max_body_size: usize,
     /// Snapshot creation period in unit of block numbers.
     snapshot_period: u64,
-
-    era: u64,
 }
 
 impl CommonParams {
@@ -45,10 +43,6 @@ impl CommonParams {
         self.snapshot_period
     }
 
-    pub fn era(&self) -> u64 {
-        self.era
-    }
-
     pub fn verify(&self) -> Result<(), String> {
         Ok(())
     }
@@ -63,30 +57,26 @@ impl CommonParams {
                 current_network_id, transaction_network_id
             ))
         }
-        if self.era < current_params.era {
-            return Err(format!("The era({}) shouldn't be less than the current era({})", self.era, current_params.era))
-        }
         Ok(())
     }
 }
 
 impl Encodable for CommonParams {
     fn rlp_append(&self, s: &mut RlpStream) {
-        s.begin_list(5)
+        s.begin_list(4)
             .append(&self.max_extra_data_size)
             .append(&self.network_id)
             .append(&self.max_body_size)
-            .append(&self.snapshot_period)
-            .append(&self.era);
+            .append(&self.snapshot_period);
     }
 }
 
 impl Decodable for CommonParams {
     fn decode(rlp: &Rlp<'_>) -> Result<Self, DecoderError> {
         let size = rlp.item_count()?;
-        if size != 5 {
+        if size != 4 {
             return Err(DecoderError::RlpIncorrectListLen {
-                expected: 5,
+                expected: 4,
                 got: size,
             })
         }
@@ -96,14 +86,11 @@ impl Decodable for CommonParams {
         let max_body_size = rlp.val_at(2)?;
         let snapshot_period = rlp.val_at(3)?;
 
-        let era = rlp.val_at(4)?;
-
         Ok(Self {
             max_extra_data_size,
             network_id,
             max_body_size,
             snapshot_period,
-            era,
         })
     }
 }
