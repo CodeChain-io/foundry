5075a5c1c80eae59fa374c17e6dbfb987dc6210f
Park Juhyung <jh@codechain.io>
2020-10-26 13:50:28 +0900

    Remove app_desc/bytes
    Using `hex` is more simple.

--- coordinator/src/app_desc/seal.rs
+++ coordinator/src/app_desc/seal.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use super::bytes::Bytes;
 use super::hash::H520;
 use serde::Deserialize;
 
@@ -36,7 +35,8 @@ pub enum Seal {
     /// Tendermint seal.
     Tendermint(TendermintSeal),
     /// Generic seal.
-    Generic(Bytes),
+    #[serde(with = "hex")]
+    Generic(Vec<u8>),
 }
 
 impl Default for Seal {
@@ -47,7 +47,6 @@ impl Default for Seal {
 
 #[cfg(test)]
 mod tests {
-    use super::super::bytes::Bytes;
     use super::super::hash::H520;
     use super::{Seal, TendermintSeal};
     use primitives::H520 as Core520;
@@ -56,23 +55,23 @@ mod tests {
     #[test]
     fn seal_deserialization() {
         let s = r#"[{
-            "generic": "0xe011bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa"
+            "generic": "e011bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa"
         },{
             "tendermint": {
                 "prev_view": 3,
                 "cur_view": 4,
                 "precommits": [
-                "0x4000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004"
+                "4000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004"
                 ]
             }
         }]"#;
 
         let deserialized: Vec<Seal> = serde_yaml::from_str(s).unwrap();
         assert_eq!(deserialized, vec![
-            Seal::Generic(Bytes::new(vec![
+            Seal::Generic(vec![
                 0xe0, 0x11, 0xbb, 0xe8, 0xdb, 0x4e, 0x34, 0x7b, 0x4e, 0x8c, 0x93, 0x7c, 0x1c, 0x83, 0x70, 0xe4, 0xb5,
                 0xed, 0x33, 0xad, 0xb3, 0xdb, 0x69, 0xcb, 0xdb, 0x7a, 0x38, 0xe1, 0xe5, 0x0b, 0x1b, 0x82, 0xfa,
-            ])),
+            ]),
             Seal::Tendermint(TendermintSeal {
                 prev_view: 0x3,
                 cur_view: 0x4,
